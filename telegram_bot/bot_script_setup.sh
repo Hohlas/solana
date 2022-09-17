9652e7cd1745fbfaad552f898b42cae18eb70501
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2022-09-17 22:56:41 +0300

    Update bot_script_setup.sh

--- telegram_bot/bot_script_setup.sh
+++ telegram_bot/bot_script_setup.sh
@@ -15,5 +15,5 @@ MAILTO=""
 */10 * * * * ~/solana/watch_main.sh
 EOF
 crontab ~/crontab.txt  # copy crontab.txt to crontab
-sudo rm ~/crontab.txt  # remoe tmp file
+sudo rm ~/crontab.txt  # remove tmp file
 sudo systemctl restart cron
