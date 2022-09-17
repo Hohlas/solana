f24f8afd13e6847043a025d9dd7c1cf94f4144f9
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2022-09-17 22:50:08 +0300

    Update bot_script_setup.sh

--- telegram_bot/bot_script_setup.sh
+++ telegram_bot/bot_script_setup.sh
@@ -3,10 +3,16 @@ sudo apt update && sudo apt install jq cron
 mkdir ~/solana && cd ~/solana
 curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_main.sh > ~/solana/watch_main.sh && chmod +x ~/solana/watch_main.sh
 curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_test.sh > ~/solana/watch_test.sh && chmod +x ~/solana/watch_test.sh
+# install solana
 sh -c "$(curl -sSfL https://release.solana.com/v1.14.1/install)" 
 export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
 echo 'export PATH='$PATH >> ~/.bashrc
 source ~/.bashrc
-sudo ln -s ~/solana/watch_main.sh /etc/cron.hourly
-sudo ln -s ~/solana/watch_test.sh /etc/cron.hourly
+# create cron
+sudo tee <<EOF >/dev/null ~/crontab.txt  
+MAILTO=""
+*/10 * * * * ~/solana/watch_test.sh 
+*/10 * * * * ~/solana/watch_main.sh
+EOF
+crontab ~/crontab.txt  # copy crontab.txt to crontab
 sudo systemctl restart cron
