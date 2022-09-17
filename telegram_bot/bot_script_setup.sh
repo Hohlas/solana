a3ecb7d41a3616cbd590f1604d37d1f9a432b4f6
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2022-09-17 21:49:14 +0300

    Update bot_script_setup.sh

--- telegram_bot/bot_script_setup.sh
+++ telegram_bot/bot_script_setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-sudo apt update && sudo apt install jq
+sudo apt update && sudo apt install jq cron
 mkdir ~/solana && cd ~/solana
-sudo apt install cron
+curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_main.sh > ~/solana/watch_main.sh && chmod +x ~/solana/watch_main.sh
+curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_test.sh > ~/solana/watch_test.sh && chmod +x ~/solana/watch_test.sh
 sudo ln -s ~/solana/watch_main.sh /etc/cron.hourly
 sudo ln -s ~/solana/watch_test.sh /etc/cron.hourly
 sudo systemctl restart cron
+sh -c "$(curl -sSfL https://release.solana.com/v1.14.1/install)" 
+export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
+echo 'export PATH='$PATH >> ~/.bashrc
+source ~/.bashrc
