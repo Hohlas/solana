a9027c73c6240da16f67af61d70fa423856da5a9
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2022-09-18 08:41:31 +0300

    Update bot_script_setup.sh

--- telegram_bot/bot_script_setup.sh
+++ telegram_bot/bot_script_setup.sh
@@ -6,18 +6,17 @@ mkdir ~/solana && cd ~/solana
 sh -c "$(curl -sSfL https://release.solana.com/v1.14.1/install)" 
 export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
 echo 'export PATH='$PATH >> ~/.bashrc
-source ~/.bashrc
+source $HOME/.bashrc
 echo "download telegram scripts"
 curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_main.sh > ~/solana/watch_main.sh && chmod +x ~/solana/watch_main.sh
 curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_test.sh > ~/solana/watch_test.sh && chmod +x ~/solana/watch_test.sh
 echo "create cron task"
-crontab -l >~/tmp.txt  # copy crontab to tmp.txt
-echo "
-MAILTO=''
+crontab -l >$HOME/tmp.txt  # copy crontab to tmp.txt
+echo "MAILTO=''
 */10 * * * * ~/solana/watch_test.sh 
 */10 * * * * ~/solana/watch_main.sh
 " >> ~/tmp.txt     # add 
-crontab ~/tmp.txt  # copy tmp.txt to crontab
-sudo rm ~/tmp.txt  # remove tmp file
+crontab $HOME/tmp.txt  # copy tmp.txt to crontab
+#sudo rm $HOME/tmp.txt  # remove tmp file
 sudo systemctl restart cron
 echo "telegram bot installed successfully"
