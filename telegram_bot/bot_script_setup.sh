36eda71f8f47a475274f2025f01dc0f9ca9878e7
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2022-09-18 08:25:30 +0300

    Update bot_script_setup.sh

--- telegram_bot/bot_script_setup.sh
+++ telegram_bot/bot_script_setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+echo "install telegram bot"
 sudo apt update && sudo apt install jq cron
+echo "install solana"
 mkdir ~/solana && cd ~/solana
-curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_main.sh > ~/solana/watch_main.sh && chmod +x ~/solana/watch_main.sh
-curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_test.sh > ~/solana/watch_test.sh && chmod +x ~/solana/watch_test.sh
-# install solana
 sh -c "$(curl -sSfL https://release.solana.com/v1.14.1/install)" 
 export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
 echo 'export PATH='$PATH >> ~/.bashrc
 source ~/.bashrc
-# create cron
+echo "download telegram scripts"
+curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_main.sh > ~/solana/watch_main.sh && chmod +x ~/solana/watch_main.sh
+curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_test.sh > ~/solana/watch_test.sh && chmod +x ~/solana/watch_test.sh
+echo "create cron task"
 crontab -l >~/tmp.txt  # copy crontab to tmp.txt
 echo "
 MAILTO=''
