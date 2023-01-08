9ed273623bbdc166140f8598daffbf807a904491
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2023-01-08 19:44:51 +0300

    Update watch_main.sh
    update BUKA IP

--- telegram_bot/watch_main.sh
+++ telegram_bot/watch_main.sh
@@ -7,7 +7,7 @@ CLUSTER=m
 #если хочешь 1 ноду то в скобках указывается только один pub,vote,ip,TEXT и т.д. Добавить можно сколько угодно нод но каждый новый параметр через пробел!  
 PUB_KEY=(5NiHw5LZn1FiL848XzbEBxuygbNvMJ7CsPvXNC8VmCLN AptafqHRpGk3KCQrGtuPGuPvWMuPc4N15X7NN7VUsfbd A4fxKaaNPBCaMwqKyhHxoWKJ5ybgvmmwTQmNmGtt2aoC)
 VOTE=(5WVvtQDDd3Gsdm3eyDrRAczP9greGmdBjNoyD93iYw9F 5rhRPFtBXDMvL95yUvirGA97cz5nWg97VREbC3CUNxao 9esjPxaUdD7yg4yDrBkP3jLipcAGVjpLDXsddF89avzW)
-IP=(185.225.191.203 146.19.24.21 185.16.39.22)
+IP=(185.16.39.34 146.19.24.21 185.16.39.22)
 # telegram bot token, chat id,text,alarm text...
 BOT_TOKEN=5076252443:AAF1rtoCAReYVY8QyZcdXGmuUOrNVICllWU
 CHAT_ALARM=-1001611695684
