88f95ba38401113dcec4750b11bd2fa946dd61cf
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-02 17:09:29 +0300

    Create get_serv_ip.sh

---
+++ get_serv_ip.sh
@@ -0,0 +1,14 @@
+# # #   get IP to $SERV   # # #
+#!/bin/bash
+source $HOME/.bashrc
+if [ -f ~/keys/ssh.key ]; then 
+chmod 600 ~/keys/ssh.key; 
+else 
+# echo -e "\033[1;31m Warning: there is no any ssh.key file in ~/keys \033[0m"
+fi
+addr='main'
+if [[ $NODE == "main" ]]; then addr='test'; fi
+echo 'addr='$addr
+tmp=$(curl https://raw.githubusercontent.com/Hohlas/solana/main/$addr/${NAME,,})
+SERV=$(echo "$tmp" | grep -o 'SERV=[^ ]*' | cut -d '=' -f2)
+echo "$SERV"
