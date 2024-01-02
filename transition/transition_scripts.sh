14e3720cfea8c181cf6667f5b8b3fb66995b4bec
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-03 00:19:59 +0300

    Update transition_scripts.sh

--- transition/transition_scripts.sh
+++ transition/transition_scripts.sh
@@ -1,10 +1,12 @@
-
 # # #   send tower to remote $SERV   # # #
 sudo tee <<EOF >/dev/null ~/tower_out.sh
 #!/bin/bash
 source $HOME/.bashrc
+if [ -f ~/keys/ssh.key ]; then chmod 600 ~/keys/ssh.key
+else echo -e '\033[31m - WARNING !!! no file ssh.key in ~/keys - \033[0m'
+fi 
 solana-validator -l ~/solana/ledger wait-for-restart-window --min-idle-time 5 --skip-new-snapshot-check
-scp -P 2010 -i /root/keys/ssh.key /root/solana/ledger/tower-1_9-$(solana-keygen pubkey ~/solana/validator-keypair.json).bin root@$SERV:/root/solana/ledger
+scp -P 2010 -i /root/keys/ssh.key /root/solana/ledger/tower-1_9-\$(solana-keygen pubkey ~/solana/validator-keypair.json).bin root@$SERV:/root/solana/ledger
 echo 'send tower to '$SERV
 EOF
 chmod +x ~/tower_out.sh
@@ -13,8 +15,11 @@ chmod +x ~/tower_out.sh
 sudo tee <<EOF >/dev/null ~/tower_in.sh
 #!/bin/bash
 source $HOME/.bashrc
+if [ -f ~/keys/ssh.key ]; then chmod 600 ~/keys/ssh.key
+else echo -e '\033[31m - WARNING !!! no file ssh.key in ~/keys - \033[0m'
+fi
 solana-validator -l ~/solana/ledger wait-for-restart-window --min-idle-time 5 --skip-new-snapshot-check
-scp -P 2010 -i /root/keys/ssh.key $SERV:/root/solana/ledger/tower-1_9-$(solana-keygen pubkey ~/solana/validator-keypair.json).bin /root/solana/ledger
+scp -P 2010 -i /root/keys/ssh.key $SERV:/root/solana/ledger/tower-1_9-\$(solana-keygen pubkey ~/solana/validator-keypair.json).bin /root/solana/ledger
 # ~/vote_on.sh
 echo 'get tower from '$SERV
 EOF
@@ -54,7 +59,6 @@ link=\$(solana address -k ~/solana/validator_link.json)
 validator=\$(solana address -k ~/solana/validator-keypair.json)
 vote=\$(solana address -k ~/solana/vote.json)
 echo '--'
-echo \$NODE'.'\$NAME ## 
 echo 'epmty_validator: '\$empty
 echo 'validator_link: '\$link
 echo 'validator: '\$validator
@@ -64,4 +68,3 @@ if [[ \$link == \$empty ]]; then echo 'link=empty: voting OFF'; fi
 if [[ \$link == \$validator ]]; then echo 'link=validator: voting ON'; fi
 EOF
 chmod +x ~/address.sh
-### ###
