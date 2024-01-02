9a40ec51043a263fe137af73fb343edb5422d3ae
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-02 21:27:48 +0300

    Update address.sh

--- address.sh
+++ address.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 source $HOME/.bashrc
-empty=\$(solana address -k ~/solana/empty-validator.json)
-link=\$(solana address -k ~/solana/validator_link.json)
-validator=\$(solana address -k ~/solana/validator-keypair.json)
-vote=\$(solana address -k ~/solana/vote.json)
+empty=$(solana address -k ~/solana/empty-validator.json)
+link=$(solana address -k ~/solana/validator_link.json)
+validator=$(solana address -k ~/solana/validator-keypair.json)
+vote=$(solana address -k ~/solana/vote.json)
 echo '--'
-echo 'epmty_validator: '\$empty
-echo 'validator_link: '\$link
-echo 'validator: '\$validator
-echo 'vote: '\$vote
+echo 'epmty_validator: '$empty
+echo 'validator_link: '$link
+echo 'validator: '$validator
+echo 'vote: '$vote
 echo '--'
-if [[ \$link == \$empty ]]; then echo 'link=empty: voting OFF'; fi
-if [[ \$link == \$validator ]]; then echo 'link=validator: voting ON'; fi
+if [[ $link == $empty ]]; then echo 'link=empty: voting OFF'; fi
+if [[ $link == $validator ]]; then echo 'link=validator: voting ON'; fi
