8ef9fd31af5d844dde567c6359a49a5249deb5c4
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-02 17:11:09 +0300

    Update get_serv_ip.sh

--- get_serv_ip.sh
+++ get_serv_ip.sh
@@ -4,7 +4,7 @@ source $HOME/.bashrc
 if [ -f ~/keys/ssh.key ]; then 
 chmod 600 ~/keys/ssh.key; 
 else 
-# echo -e "\033[1;31m Warning: there is no any ssh.key file in ~/keys \033[0m"
+echo -e "\033[1;31m Warning: there is no any ssh.key file in ~/keys \033[0m"
 fi
 addr='main'
 if [[ $NODE == "main" ]]; then addr='test'; fi
