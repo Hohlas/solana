87f6266b43cdfec7539be0d5b826077ab50b49b4
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-04 13:35:32 +0300

    Update transition_scripts.sh
    # copy this line to primary server

--- transition_scripts.sh
+++ transition_scripts.sh
@@ -97,7 +97,7 @@ if [ \$networkrpcURL = https://api.testnet.solana.com ]; then net="api.testnet";
 elif [ \$networkrpcURL = https://api.mainnet-beta.solana.com ]; then net="api.mainnet-beta";
 fi	
 echo \$NODE'.'\$NAME 'network='\$net
-echo 'SERV=root@'\$(wget -q -4 -O- http://icanhazip.com)
+echo 'SERV=root@'\$(wget -q -4 -O- http://icanhazip.com)'  # copy this line to primary server'
 EOF
 chmod +x ~/check.sh
 
