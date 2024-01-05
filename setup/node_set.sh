aa9031bd1897efe869f65b58670be2fcf9e9930d
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-05 22:26:40 +0300

    Create node_set.sh

---
+++ setup/node_set.sh
@@ -0,0 +1,17 @@
+echo 'node='$NODE ' name='$NAME
+curl https://raw.githubusercontent.com/Hohlas/solana/main/$NODE/${NAME,,} >> $HOME/.bashrc
+if [[ $NODE == "main" ]]; then 
+solana config set --url https://api.mainnet-beta.solana.com --keypair ~/solana/validator-keypair.json
+$GIT/Jito/solana.service > ~/solana/solana.service
+~/vote_off.sh
+echo -e "\033[31m set MAIN $NAME\033[0m"
+elif [[ $NODE == "test" ]]; then
+solana config set --url https://api.testnet.solana.com --keypair ~/solana/validator-keypair.json
+$GIT/test/solana.service > ~/solana/solana.service
+~/vote_on.sh
+echo -e "\033[34m set test $NAME\033[0m"
+else
+echo -e "\033[31m Warning, unknown node type: $NODE \033[0m"
+fi
+systemctl daemon-reload
+~/check.sh
