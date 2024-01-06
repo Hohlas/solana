a7a299394b93c5933a607db55f8468e30b6a4e50
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-06 10:38:50 +0300

    Update settings.sh
    add grafana setup

--- setup/settings.sh
+++ setup/settings.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+echo -e '\n\e[42m Run solana settings \e[0m\n'
 # create dirs
 mkdir -p ~/solana/ledger
 mkdir -p /mnt/disk3/accounts
@@ -37,17 +39,21 @@ export GIT='curl https://raw.githubusercontent.com/Hohlas/solana/main'
 echo 'export GIT="'$GIT'"' >> $HOME/.bashrc
 source $HOME/.bashrc
 # download settings and scripts
+echo -e '\n\e[42m Download scripts \e[0m\n'
 $GIT/setup/21-solana-validator.conf > /etc/sysctl.d/21-solana-validator.conf
 $GIT/setup/90-solana-nofiles.conf > /etc/security/limits.d/90-solana-nofiles.conf
 $GIT/setup/solana.logrotate > /etc/logrotate.d/solana.logrotate
+$GIT/setup/grafana_sutup.sh > ~/grafana_sutup.sh
+$GIT/setup/grafana_update.sh > ~/grafana_update.sh
 $GIT/setup/check.sh > ~/check.sh
 $GIT/setup/tower.sh > ~/tower.sh
 $GIT/setup/vote_on.sh > ~/vote_on.sh
 $GIT/setup/vote_off.sh > ~/vote_off.sh
 $GIT/setup/next.sh > ~/next.sh
 $GIT/setup/node_set.sh > ~/node_set.sh
-chmod +x ~/check.sh ~/tower.sh ~/vote_on.sh ~/vote_off.sh ~/next.sh ~/node_set.sh
+chmod +x ~/grafana_sutup.sh ~/grafana_update.sh ~/check.sh ~/tower.sh ~/vote_on.sh ~/vote_off.sh ~/next.sh ~/node_set.sh
 sudo sysctl -p /etc/sysctl.d/21-solana-validator.conf
 sudo systemctl daemon-reload
 sudo systemctl restart logrotate
-# # #
+~/grafana_sutup.sh 
+source ~/grafana_update.sh
