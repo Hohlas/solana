ed7d5fa34a13c42c9dd1c4e93a9e17731e5b4219
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-06 14:34:57 +0300

    Update settings.sh
    setup by ~/sol_git

--- setup/settings.sh
+++ setup/settings.sh
@@ -20,15 +20,15 @@ ln -sf ~/solana/empty-validator.json ~/solana/validator_link.json # NO voting mo
 echo ' # SOLANA  ALIAS # ' >> $HOME/.bashrc
 echo "alias mount_keys='encfs ~/.crpt ~/keys'" >> $HOME/.bashrc
 echo "alias umount_keys='fusermount -u ~/keys'" >> $HOME/.bashrc
-echo "alias check='source ~/check.sh'" >> $HOME/.bashrc
-echo 'alias tower=~/tower.sh' >> $HOME/.bashrc
-echo 'alias vote_on=~/vote_on.sh' >> $HOME/.bashrc
-echo 'alias vote_off=~/vote_off.sh' >> $HOME/.bashrc
+echo "alias check='source ~/sol_git/setup/check.sh'" >> $HOME/.bashrc
+echo 'alias tower=~/sol_git/setup/tower.sh' >> $HOME/.bashrc
+echo 'alias vote_on=~/sol_git/setup/vote_on.sh' >> $HOME/.bashrc
+echo 'alias vote_off=~/sol_git/setup/vote_off.sh' >> $HOME/.bashrc
 echo "alias logs='tail -f ~/solana/solana.log'" >> $HOME/.bashrc
 echo "alias catch='solana catchup ~/solana/validator_link.json --our-localhost --follow --log'" >> $HOME/.bashrc
 echo "alias monitor='solana-validator --ledger ~/solana/ledger monitor'" >> $HOME/.bashrc
 echo 'alias next=~/next.sh' >> $HOME/.bashrc
-echo "alias node_set='source ~/node_set.sh'" >> $HOME/.bashrc
+echo "alias node_set='source ~/sol_git/setup/node_set.sh'" >> $HOME/.bashrc
 echo ' # --- # ' >> $HOME/.bashrc
 
 # add PATH
@@ -36,24 +36,16 @@ if ! echo $PATH | grep -q "$HOME/.local/share/solana/install/active_release/bin"
 export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
 echo 'export PATH='$PATH >> ~/.bashrc
 fi
-export GIT='curl https://raw.githubusercontent.com/Hohlas/solana/main'
-echo 'export GIT="'$GIT'"' >> $HOME/.bashrc
 source $HOME/.bashrc
 # download settings and scripts
-echo -e '\n\e[42m Download scripts \e[0m\n'
-$GIT/setup/21-solana-validator.conf > /etc/sysctl.d/21-solana-validator.conf
-$GIT/setup/90-solana-nofiles.conf > /etc/security/limits.d/90-solana-nofiles.conf
-$GIT/setup/solana.logrotate > /etc/logrotate.d/solana.logrotate
-$GIT/setup/grafana_setup.sh > ~/grafana_setup.sh
-$GIT/setup/check.sh > ~/check.sh
-$GIT/setup/tower.sh > ~/tower.sh
-$GIT/setup/vote_on.sh > ~/vote_on.sh
-$GIT/setup/vote_off.sh > ~/vote_off.sh
-$GIT/setup/next.sh > ~/next.sh
-$GIT/setup/node_set.sh > ~/node_set.sh
-chmod +x ~/grafana_setup.sh ~/check.sh ~/tower.sh ~/vote_on.sh ~/vote_off.sh ~/next.sh ~/node_set.sh
-sudo sysctl -p /etc/sysctl.d/21-solana-validator.conf
-sudo systemctl daemon-reload
-sudo systemctl restart logrotate
-~/grafana_setup.sh
+echo -e '\n\e[42m system config \e[0m\n'
+~/sol_git/setup/21-solana-validator.conf > /etc/sysctl.d/21-solana-validator.conf
+~/sol_git/setup/90-solana-nofiles.conf > /etc/security/limits.d/90-solana-nofiles.conf
+~/sol_git/setup/solana.logrotate > /etc/logrotate.d/solana.logrotate
+sysctl -p /etc/sysctl.d/21-solana-validator.conf
+systemctl restart logrotate
+systemctl daemon-reload
+cd ~/sol_git/setup
+chmod +x ./grafana_setup.sh ./check.sh ./tower.sh ./vote_on.sh ./vote_off.sh ./next.sh ./node_set.sh
+./grafana_setup.sh 
 echo -e '\n\e[42m Solana setup complete \e[0m\n'
