6c4c6629b174a83cfe79d9aabe6532eb9514bdc4
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-06 12:55:28 +0300

    Update check.sh
    echo "v$version - $client"

--- setup/check.sh
+++ setup/check.sh
@@ -27,6 +27,10 @@ elif [ $rpcURL = https://api.mainnet-beta.solana.com ]; then
 echo -e "\033[31m network=api.mainnet-beta \033[0m";
 fi	
 
+version=$(solana --version | awk '{print $2}')
+client=$(solana --version | awk -F'client:' '{print $2}' | tr -d ')')
+echo "v$version - $client"
+
 echo ' ~/tower.sh '`whoami`'@'$(wget -q -4 -O- http://icanhazip.com)'  # run it on Primary server'
 if [[ $link == $empty ]]; then echo -e "\033[32m vote OFF\033[0m"; fi
 if [[ $link == $validator ]]; then echo  -e "\033[31m vote ON\033[0m"; fi
