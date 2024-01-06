ddc17ec08869a2f605a4f6e5d5b44297e5c15ace
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-06 10:55:56 +0300

    Update grafana_sutup.sh

--- setup/grafana_sutup.sh
+++ setup/grafana_sutup.sh
@@ -27,3 +27,9 @@ if [ ! -e /etc/default/locale ];
 then curl https://raw.githubusercontent.com/Hohlas/ubuntu/main/crypto/locale > /etc/default/locale; 
 echo "Download locale file to /etc/default"; 
 fi # файл locale иногда отсутствует, из-за этого появляется ошибка
+
+echo -e '\n\e[42m dowload telegraf.conf \e[0m\n'
+curl https://raw.githubusercontent.com/Hohlas/solana/main/setup/telegraf.conf > /etc/telegraf/telegraf.conf
+sudo systemctl daemon-reload 
+sudo systemctl enable telegraf 
+sudo systemctl restart telegraf 
