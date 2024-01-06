b82c19fdc423c3e99940d3f14dfb6c8158b76685
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-06 15:45:45 +0300

    Update node_set.sh
    add ~/sol_git

--- setup/node_set.sh
+++ setup/node_set.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # # #   set MAIN/TEST settings
+cd ~/sol_git; git pull;
 source $HOME/.bashrc
-$GIT/$NODE/${NAME,,} >> $HOME/.bashrc
+cat ~/sol_git/$NODE/${NAME,,} >> $HOME/.bashrc
 if [[ $NODE == "main" ]]; then 
 solana config set --url https://api.mainnet-beta.solana.com --keypair ~/solana/validator-keypair.json
-$GIT/Jito/solana.service > ~/solana/solana.service
+cp ~/sol_git/Jito/solana.service ~/solana/solana.service
 ~/vote_off.sh
 echo -e "\033[31m set MAIN $NAME\033[0m"
 elif [[ $NODE == "test" ]]; then
 solana config set --url https://api.testnet.solana.com --keypair ~/solana/validator-keypair.json
-$GIT/test/solana.service > ~/solana/solana.service
+cp ~/sol_git/test/solana.service ~/solana/solana.service
 ~/vote_on.sh
 echo -e "\033[34m set test $NAME\033[0m"
 else
@@ -17,4 +18,3 @@ echo -e "\033[31m Warning, unknown node type: $NODE \033[0m"
 fi
 systemctl daemon-reload
 ~/check.sh
-# # # # #
