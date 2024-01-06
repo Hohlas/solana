2eda8ccf7a97d56bd8bfee9268ad5a063409715f
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-06 10:25:20 +0300

    Create grafana_sutup.sh

---
+++ setup/grafana_sutup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ulimit -n 1000000
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D8FF8E1F7DF8B07E
+
+cd
+apt install gnupg -y # gnupg2 gnupg1 -y
+cat <<EOF | sudo tee /etc/apt/sources.list.d/influxdata.list
+deb https://repos.influxdata.com/ubuntu focal stable
+EOF
+sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+sudo apt-get update && sudo apt -y install telegraf git jq bc -y
+sudo systemctl enable --now telegraf
+sudo systemctl is-enabled telegraf
+systemctl status telegraf
+
+sudo adduser telegraf sudo
+sudo adduser telegraf adm
+sudo -- bash -c 'echo "telegraf ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
+sudo cp /etc/telegraf/telegraf.conf /etc/telegraf/telegraf.conf.orig
+sudo rm -rf /etc/telegraf/telegraf.conf
+cd ~/ 
+git clone https://github.com/stakeconomy/solanamonitoring/  
+chmod +x ~/solanamonitoring/monitor.sh
+if [ ! -e /etc/default/locale ]; 
+then curl https://raw.githubusercontent.com/Hohlas/ubuntu/main/crypto/locale > /etc/default/locale; 
+echo "Download locale file to /etc/default"; 
+fi # файл locale иногда отсутствует, из-за этого появляется ошибка
