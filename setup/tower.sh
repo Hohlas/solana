20b819f3b868542dfa6f2a7f81dd0f0d2baf654f
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-08 19:49:08 +0300

    Update tower.sh
    add ssh agent

--- setup/tower.sh
+++ setup/tower.sh
@@ -7,25 +7,33 @@ if [ -z "$1" ]; then
 fi
 DIR=$1  # transfer direction ('to' / 'from')
 SERV=$2 # transfer server addr (root@xxx.xx.xx.xx)
-echo 'try to connect to '$SERV
+
+# ssh connection
 if [ -f ~/keys/*.ssh ]; then chmod 600 ~/keys/*.ssh
 else echo -e '\033[31m - WARNING !!! no any *.ssh files in ~/keys - \033[0m'
 fi 
+# ask ssh password once
+eval "$(ssh-agent -s)"  # Start ssh-agent in the background
+ssh-add ~/keys/test.ssh # Add SSH private key to the ssh-agent
+
 # wait for window
 solana-validator -l ~/solana/ledger wait-for-restart-window --min-idle-time 10 --skip-new-snapshot-check
+
 # read current keys status
 empty=$(solana address -k ~/solana/empty-validator.json)
 link=$(solana address -k ~/solana/validator_link.json)
 validator=$(solana address -k ~/solana/validator-keypair.json)
-# get tower from Secondary server in 'voting OFF' mode
+
+# get tower from Secondary server
 if [[ $DIR == 'from' ]]; then 
-echo ' get tower from '$SERV; 
+echo -e "\033[31m get tower from\033[0m" $SERV; 
 read -p "are you ready? " RESP; if [ "$RESP" != "y" ]; then exit 1; fi
 scp -P 2010 -i /root/keys/*.ssh $SERV:/root/solana/ledger/tower-1_9-$(solana-keygen pubkey ~/solana/validator-keypair.json).bin /root/solana/ledger
 fi
-# send tower to Secondary server in 'voting ON' mode
+
+# send tower to Secondary server
 if [[ $DIR == 'to' ]]; then 
-echo ' send tower to '$SERV; 
+echo -e "\033[32m send tower to\033[0m" $SERV; 
 read -p "are you ready? " RESP; if [ "$RESP" != "y" ]; then exit 1; fi
 scp -P 2010 -i /root/keys/*.ssh /root/solana/ledger/tower-1_9-$(solana-keygen pubkey ~/solana/validator-keypair.json).bin $SERV:/root/solana/ledger
 fi
