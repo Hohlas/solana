0c455a481fb1b9fa0f09ca53698cd13a3b809719
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-10 18:31:00 +0300

    Update settings.sh

--- setup/settings.sh
+++ setup/settings.sh
@@ -5,30 +5,22 @@ mkdir -p ~/solana/ledger
 mkdir -p /mnt/disk3/accounts
 mkdir -p /mnt/ramdisk/accounts_hash_cache
 
-# create no voting empty validator
-if [ ! -f ~/solana/empty-validator.json ]; then 
-solana-keygen new -s --no-bip39-passphrase -o ~/solana/empty-validator.json
-fi
-
-# create links
+# create service link
 ln -sf ~/solana/solana.service /etc/systemd/system
-ln -sf ~/keys/*${NODE}_vote.json ~/solana/vote.json
-ln -sf ~/keys/*${NODE}_validator.json ~/solana/validator-keypair.json
-ln -sf ~/solana/empty-validator.json ~/solana/validator_link.json # NO voting mode
 
 # create alias #
 echo ' # SOLANA  ALIAS # ' >> $HOME/.bashrc
 echo "alias mount_keys='encfs ~/.crpt ~/keys'" >> $HOME/.bashrc
 echo "alias umount_keys='fusermount -u ~/keys'" >> $HOME/.bashrc
 echo "alias check='source ~/sol_git/setup/check.sh'" >> $HOME/.bashrc
-echo 'alias tower=~/sol_git/setup/tower.sh' >> $HOME/.bashrc
-echo 'alias vote_on=~/sol_git/setup/vote_on.sh' >> $HOME/.bashrc
+echo "alias tower='source ~/sol_git/setup/tower.sh'" >> $HOME/.bashrc
+echo "alias vote_on='source ~/sol_git/setup/vote_on.sh'" >> $HOME/.bashrc
 echo 'alias vote_off=~/sol_git/setup/vote_off.sh' >> $HOME/.bashrc
 echo "alias logs='tail -f ~/solana/solana.log'" >> $HOME/.bashrc
 echo "alias catch='solana catchup ~/solana/validator_link.json --our-localhost --follow --log'" >> $HOME/.bashrc
 echo "alias monitor='solana-validator --ledger ~/solana/ledger monitor'" >> $HOME/.bashrc
 echo 'alias next=~/sol_git/setup/next.sh' >> $HOME/.bashrc
-echo "alias node_set='source ~/sol_git/setup/node_set.sh'" >> $HOME/.bashrc
+echo 'alias node_set=~/sol_git/setup/node_set.sh' >> $HOME/.bashrc
 echo ' # --- # ' >> $HOME/.bashrc
 
 # add PATH
@@ -36,7 +28,6 @@ if ! grep -q "$HOME/.local/share/solana/install/active_release/bin" ~/.bashrc; t
     export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
     echo 'export PATH='$PATH >> ~/.bashrc
 fi
-source $HOME/.bashrc
 # download settings and scripts
 echo -e '\n\e[42m system config \e[0m\n'
 cp ~/sol_git/setup/21-solana-validator.conf /etc/sysctl.d/21-solana-validator.conf
@@ -47,5 +38,5 @@ sysctl -p /etc/sysctl.d/21-solana-validator.conf
 systemctl restart logrotate
 systemctl daemon-reload
 chmod +x ~/sol_git/setup/*.sh
-source ./grafana_setup.sh 
+~/sol_git/grafana_setup.sh 
 echo -e '\n\e[42m Solana setup complete \e[0m\n'
