fbff2f2b50f59304e036deeb722bd7038cacc479
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-10 15:58:02 +0300

    Update node_set.sh
    echo 'export TAG='$TAG >> $HOME/.bashrc

--- setup/node_set.sh
+++ setup/node_set.sh
@@ -14,6 +14,10 @@ source $HOME/.bashrc
 # cat ~/sol_git/$NODE/${NAME,,} >> $HOME/.bashrc
 ln -sf ~/keys/${NAME,,}_${NODE}_vote.json ~/solana/vote.json
 ln -sf ~/keys/${NAME,,}_${NODE}_validator.json ~/solana/validator-keypair.json
+echo '# --- #' >> $HOME/.bashrc
+echo 'export TAG='$TAG >> $HOME/.bashrc
+echo 'export NODE='$NODE >> $HOME/.bashrc
+echo 'export NAME='$NAME >> $HOME/.bashrc
 echo 'export validator_key='$(solana address -k ~/solana/validator-keypair.json) >> $HOME/.bashrc
 echo 'export vote_account='$(solana address -k ~/solana/vote.json) >> $HOME/.bashrc
 tmp="\"$NAME.$NODE\""
