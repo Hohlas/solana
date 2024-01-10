675750df62f96c844c7449f2ce53e284bde82626
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-10 18:28:10 +0300

    Update vote_on.sh

--- setup/vote_on.sh
+++ setup/vote_on.sh
@@ -3,5 +3,7 @@
 source $HOME/.bashrc
 solana-validator -l ~/solana/ledger set-identity --require-tower ~/solana/validator-keypair.json
 ln -sfn ~/solana/validator-keypair.json ~/solana/validator_link.json
-systemctl restart telegraf
+# update telegraf
+sed -i "/^  hostname = /c\  hostname = \"$NAME\"" /etc/telegraf/telegraf.conf
+systemctl start telegraf
 echo -e "\033[31m vote ON\033[0m"
