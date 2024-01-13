b6a917b7e6a9987bc844ab88824b7d7cd0f025a0
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-13 11:42:54 +0300

    Update snap.sh

--- setup/snap.sh
+++ setup/snap.sh
@@ -1,3 +1,4 @@
-mkdir -p ~/solana/snapshots
-SERV=$1 #
-rsync -a -e "ssh -p 2010 -i ~/keys/test.ssh" --progress --include '*snapshot*' --exclude '*' $SERV:~/solana/ledger/ ~/solana/snapshots
+#!/bin/bash
+rsync -a -e "ssh -p 2010 -i ~/keys/test.ssh" \
+--progress --include '*snapshot*' --exclude '*' \
+$SERV:~/solana/ledger/ ~/solana/ledger
