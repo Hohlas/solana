affb23f3daef1b3313d5fa2ad0a3ef60cf568f9b
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-13 12:02:25 +0300

    Create ssh_agent.sh

---
+++ setup/ssh_agent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# used for public key authentication. 
+# Once you add your private key to ssh-agent, 
+# you won’t need to enter your passphrase every time.
+
+eval "$(ssh-agent -s)"  # Start ssh-agent in the background
+ssh-add ~/keys/test.ssh # Add SSH private key to the ssh-agent
