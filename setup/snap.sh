857d85ae5c329825d516d6422fe995b04862f464
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-16 19:16:37 +0300

    Update snap.sh

--- setup/snap.sh
+++ setup/snap.sh
@@ -2,11 +2,11 @@
 DIR=$1
 SERV=$2
 if [[ $DIR == "to" ]]; then   # LOCAL -> REMOTE
-rsync -a -e "ssh -p 2010 -i ~/keys/test.ssh" \
---progress \
+rsync -a -e "ssh -p 2010 -i ~/keys/test.ssh" --progress \
 ~/solana/snapshots $SERV:~/solana/
 else                          # REMOTE -> LOCAL
-rsync -a -e "ssh -p 2010 -i ~/keys/test.ssh" \
---progress \
+rsync -a -e "ssh -p 2010 -i ~/keys/test.ssh" --progress \
 $SERV:~/solana/snapshots ~/solana/
 fi
+
+# rsync -a -e "ssh -p 2010 -i ~/keys/test.ssh" --progress ~/solana/ledger/*snapshot* $SERV:~/solana/ledger/
