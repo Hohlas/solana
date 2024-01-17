4dedc9ffcce8411574ae898fe5ba8d566c15a73f
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-17 14:53:07 +0300

    Update snap.sh
    key=$(ls ~/keys/*.ssh)  # for any *.ssh file in ~/keys

--- setup/snap.sh
+++ setup/snap.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 DIR=$1
 SERV=$2
+key=$(ls ~/keys/*.ssh) # any *.ssh file in ~/keys
 if [[ $DIR == "to" ]]; then   # LOCAL -> REMOTE
-rsync -a -e "ssh -p 2010 -i ~/keys/test.ssh" --progress ~/solana/snapshots $SERV:~/solana/
-# rsync -a -e "ssh -p 2010 -i ~/keys/test.ssh" --progress ~/solana/ledger/*snapshot-* $SERV:~/solana/ledger/
+rsync -a -e "ssh -p 2010 -i $key" --progress ~/solana/snapshots $SERV:~/solana/
+# rsync -a -e "ssh -p 2010 -i $key" --progress ~/solana/ledger/*snapshot-* $SERV:~/solana/ledger/
 else                          # REMOTE -> LOCAL
-rsync -a -e "ssh -p 2010 -i ~/keys/test.ssh" --progress $SERV:~/solana/snapshots ~/solana/
-# rsync -a -e "ssh -p 2010 -i ~/keys/test.ssh" --progress $SERV:~/solana/ledger/*snapshot-* ~/solana/ledger/
+rsync -a -e "ssh -p 2010 -i $key" --progress $SERV:~/solana/snapshots ~/solana/
+# rsync -a -e "ssh -p 2010 -i $key" --progress $SERV:~/solana/ledger/*snapshot-* ~/solana/ledger/
 fi
