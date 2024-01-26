665a40471069329b2f3a0ea9334c8b97cbf77f85
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-26 16:21:14 +0300

    Update vote_on.sh

--- setup/vote_on.sh
+++ setup/vote_on.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # # #   Start Voting   # # # # # # # # # # # # # # # # # # # # #
 source $HOME/.bashrc
-solana-validator -l ~/solana/ledger set-identity --require-tower ~/solana/validator-keypair.json
+if [ -f ~/solana/ledger/tower-1_9-$(solana-keygen pubkey ~/solana/validator-keypair.json).bin ]; 
+then 
+echo -e "\033[32m tower exist\033[0m";
+solana-validator -l ~/solana/ledger set-identity --require-tower ~/solana/validator-keypair.json; 
+else
+echo -e "\033[31m without tower\033[0m";
+solana-validator -l ~/solana/ledger set-identity ~/solana/validator-keypair.json;
+fi
 ln -sfn ~/solana/validator-keypair.json ~/solana/validator_link.json
 # update telegraf
 sed -i "/^  hostname = /c\  hostname = \"$NAME\"" /etc/telegraf/telegraf.conf
