ed4669584d1c254c1534e45561372dba5ccc300c
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-01-28 13:49:19 +0300

    Update check.sh

--- setup/check.sh
+++ setup/check.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # # #   check address   # # # # # # # # # # # # # # # # # # # # #
+rpcURL=$(solana config get | grep "RPC URL" | awk '{print $3}')
+version=$(solana --version | awk '{print $2}')
+client=$(solana --version | awk -F'client:' '{print $2}' | tr -d ')')
 empty=$(solana address -k ~/solana/empty-validator.json)
 link=$(solana address -k ~/solana/validator_link.json)
-validator=$(solana address -k ~/solana/validator-keypair.json)
+validator=$(solana address --url $rpcURL) #$(solana address -k ~/solana/validator-keypair.json)
 vote=$(solana address -k ~/solana/vote.json)
 echo '--'
 echo 'epmty_validator: '$empty
@@ -11,30 +14,27 @@ echo 'validator: '$validator
 echo 'vote: '$vote
 echo '--'
 
-if [[ $NODE == "main" ]]; then 
-echo -e "\033[31m "$NODE'.'$NAME" \033[0m"; 
-elif [[ $NODE == "test" ]]; then
-echo -e "\033[34m "$NODE'.'$NAME" \033[0m"; 
-else
-echo -e "\033[31m Warning, unknown node type: $NODE \033[0m"
-fi
-
-rpcURL=$(solana config get | grep "RPC URL" | awk '{print $3}')
 if [ $rpcURL = https://api.testnet.solana.com ]; then 
+echo -e "\033[34m "$NODE'.'$NAME" \033[0m";
 echo -e "\033[34m network=api.testnet \033[0m";
 elif [ $rpcURL = https://api.mainnet-beta.solana.com ]; then 
+echo -e "\033[31m "$NODE'.'$NAME" \033[0m";
 echo -e "\033[31m network=api.mainnet-beta \033[0m";
 fi	
-
-version=$(solana --version | awk '{print $2}')
-client=$(solana --version | awk -F'client:' '{print $2}' | tr -d ')')
 echo "v$version - $client"
 
 if [[ $link == $empty ]]; then 
 echo ' tower to '`whoami`'@'$(wget -q -4 -O- http://icanhazip.com)'  # run it on Primary server'	
-echo -e "\033[32m vote OFF\033[0m"; 
+echo -e "\033[32m validator=empty\033[0m"; 
 fi
 if [[ $link == $validator ]]; then 
 echo ' tower from '`whoami`'@'$(wget -q -4 -O- http://icanhazip.com)'  # run it on Primary server'
-echo  -e "\033[31m vote ON\033[0m"; 
+echo  -e "\033[31m validator=true\033[0m"; 
+fi
+
+DELINQUEENT=$(solana validators --url $rpcURL --output json-compact | jq '.validators[] | select(.identityPubkey == "'"${validator}"'" ) | .delinquent ')
+if [[ $DELINQUEENT == true ]]; then 
+echo -e "\033[32m vote OFF\033[0m";
+else
+echo -e "\033[31m vote ON\033[0m"; 
 fi
