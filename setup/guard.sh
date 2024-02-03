d212c103524ba3e2bd4452a3e24b1af594195ea0
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-02-04 00:05:41 +0300

    Update guard.sh

--- setup/guard.sh
+++ setup/guard.sh
@@ -60,13 +60,14 @@ echo "  set empty identity on REMOTE server "
 ssh REMOTE $SOL/solana-validator -l ~/solana/ledger set-identity ~/solana/empty-validator.json
 echo "  change validator link on REMOTE server "  
 ssh REMOTE ln -sf ~/solana/empty-validator.json ~/solana/validator_link.json
-echo "  stop telegraf"
+echo "  stop solana and telegraf on REMOTE server"
 ssh REMOTE systemctl stop telegraf
-echo -e "\033[32m  restart solana on REMOTE server in NO_VOTING mode \033[0m"
-ssh REMOTE systemctl restart solana
+ssh REMOTE systemctl stop solana
 echo "  move tower from REMOTE to LOCAL "
 scp -P 2010 -i /root/keys/$NAME.ssh $SERV:/root/solana/ledger/tower-1_9-$PUB_KEY.bin /root/solana/ledger
-ssh REMOTE rm /root/solana/ledger/tower-1_9-$PUB_KEY.bin
+# ssh REMOTE rm /root/solana/ledger/tower-1_9-$PUB_KEY.bin
+echo -e "\033[32m  restart solana on REMOTE server in NO_VOTING mode \033[0m"
+ssh REMOTE systemctl start solana
 
 # START SOLANA on LOCAL server
 if [ -f ~/solana/ledger/tower-1_9-$PUB_KEY.bin ]; then 
