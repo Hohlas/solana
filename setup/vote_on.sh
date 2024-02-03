af6452c78217c59a367676a577f15bfcfceab5b0
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-02-03 11:41:42 +0300

    Update vote_on.sh

--- setup/vote_on.sh
+++ setup/vote_on.sh
@@ -4,22 +4,24 @@ source $HOME/.bashrc
 rpcURL=$(solana config get | grep "RPC URL" | awk '{print $3}')
 PUB_KEY=$(solana-keygen pubkey ~/solana/validator-keypair.json)
 
-Delinquent=false
-until [[ $Delinquent == true ]]; do
-JSON=$(solana validators --url $rpcURL --output json-compact 2>/dev/null | jq '.validators[] | select(.identityPubkey == "'"${PUB_KEY}"'" )')
-LastVote=$(echo "$JSON" | jq -r '.lastVote')
-Delinquent=$(echo "$JSON" | jq -r '.delinquent')
-echo -ne "Looking for "$PUB_KEY". LastVote="$LastVote" \r"
-sleep 3
-done
+FORCE=$1
+if [ -z "$FORCE" ] # no 'force' flag, so waiting for 'delink' status
+  Delinquent=false
+  until [[ $Delinquent == true ]]; do
+    JSON=$(solana validators --url $rpcURL --output json-compact 2>/dev/null | jq '.validators[] | select(.identityPubkey == "'"${PUB_KEY}"'" )')
+    LastVote=$(echo "$JSON" | jq -r '.lastVote')
+    Delinquent=$(echo "$JSON" | jq -r '.delinquent')
+    echo -ne "Looking for "$PUB_KEY". LastVote="$LastVote" \r"
+    sleep 3
+  done
+fi
 
-if [ -f ~/solana/ledger/tower-1_9-$PUB_KEY.bin ]; 
-then 
-TOWER_STATUS=' with existing tower'
-solana-validator -l ~/solana/ledger set-identity --require-tower ~/solana/validator-keypair.json; 
+if [ -f ~/solana/ledger/tower-1_9-$PUB_KEY.bin ]; then 
+  TOWER_STATUS=' with existing tower'
+  solana-validator -l ~/solana/ledger set-identity --require-tower ~/solana/validator-keypair.json; 
 else
-TOWER_STATUS=' without tower'
-solana-validator -l ~/solana/ledger set-identity ~/solana/validator-keypair.json;
+  TOWER_STATUS=' without tower'
+  solana-validator -l ~/solana/ledger set-identity ~/solana/validator-keypair.json;
 fi
 ln -sfn ~/solana/validator-keypair.json ~/solana/validator_link.json
 # update telegraf
