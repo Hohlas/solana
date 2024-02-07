9361a00152b996a091801a24dd4fcd0665b885cc
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-02-07 17:34:28 +0300

    Update vote_off.sh

--- setup/vote_off.sh
+++ setup/vote_off.sh
@@ -10,8 +10,8 @@ ln -sf ~/solana/empty-validator.json ~/solana/validator_link.json
 command_output=$(solana-validator -l ~/solana/ledger set-identity ~/solana/empty-validator.json 2>&1)
 command_exit_status=$?
 echo $command_output 
-if [ $command_exit_status -eq 0 ]; then   echo -e "\033[32m  set empty identity successful \033[0m" 
-else                                      echo -e "\033[31m  can not set empty identity \033[0m"
+if [ $command_exit_status -eq 0 ]; then   echo -e "\033[32m set empty identity successful \033[0m" 
+else                                      echo -e "\033[31m can not set empty identity \033[0m"
 fi
 
 systemctl stop telegraf
