27f44f60bffb108b18a84f36786383fc701fbadc
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-02-07 21:16:53 +0300

    Update check.sh

--- setup/check.sh
+++ setup/check.sh
@@ -8,6 +8,9 @@ link=$(solana address -k ~/solana/validator_link.json)
 validator=$(timeout 3 stdbuf -oL solana-validator --ledger ~/solana/ledger monitor 2>/dev/null | grep -m1 Identity | awk -F': ' '{print $2}')
 PUB_KEY=$(solana address -k ~/solana/validator-keypair.json) # validator from keyfile 'validator-keypair.json'
 vote=$(solana address -k ~/solana/vote.json)
+GRAY=$"\033[90m"; GREEN=$"\033[32m"; RED=$"\033[31m"
+CUR_IP=$(wget -q -4 -O- http://icanhazip.com)
+SERV=$(solana gossip | grep $PUB_KEY | awk '{print $1}')
 
 if [ $rpcURL = https://api.testnet.solana.com ]; then 
 echo -e "\033[34m "$NODE'.'$NAME" \033[0m";
@@ -20,26 +23,30 @@ echo "v$version - $client"
 
 if [[ $validator == $empty ]]; then 
 echo ' tower to '`whoami`'@'$(wget -q -4 -O- http://icanhazip.com)'  # run it on another server'
-VAL_CLR="\033[90m" # set gray color
+VAL_CLR=$GRAY # set gray color
 elif [[ $validator == $PUB_KEY ]]; then 
 echo ' tower from '`whoami`'@'$(wget -q -4 -O- http://icanhazip.com)'  # run it on another server'
-VAL_CLR="\033[32m" # set green color
+VAL_CLR=$GREEN # set green color
 else
 echo -e "\033[31m validator="$validator", unknown status \033[0m";
 fi
-if   [[ $link == $empty ]];   then LNK_CLR="\033[90m" # set gray color
-else [[ $link == $PUB_KEY ]];      LNK_CLR="\033[32m" # set green color
+if   [[ $link == $empty ]];   then LNK_CLR=$GRAY   # set gray color
+else [[ $link == $PUB_KEY ]];      LNK_CLR=$GREEN  # set green color
 fi
 
 echo '--'
 echo ' vote account:      '$vote
-echo -e " epmty_keypair:     \033[90m"$empty"\033[0m"   # gray color
-echo -e " validator-keypair: \033[32m"$PUB_KEY"\033[0m" # green color
+echo -e " epmty_keypair:     "$GRAY$empty"\033[0m"   # gray color
+echo -e " validator-keypair: "$GREEN$PUB_KEY"\033[0m" # green color
 echo -e " validator_link:    ${LNK_CLR}"$link"\033[0m"
 echo -e " current validator: ${VAL_CLR}"$validator"\033[0m"
 echo '--'
+
+if [[ $VAL_CLR == $GRAY && $LNK_CLR == $GRAY ]];  then echo 'gray'; STATUS=$GRAY" on "$SERV" \033[0m"; fi
+if [[ $VAL_CLR == $GREEN && $LNK_CLR == $GREEN ]]; then echo 'green'; STATUS=$GREEN" on current server \033[0m"; fi
+
 DELINQUEENT=$(solana validators --url $rpcURL --output json-compact | jq '.validators[] | select(.identityPubkey == "'"${PUB_KEY}"'" ) | .delinquent ')
-if   [[ $DELINQUEENT == true ]];  then echo -e "\033[31m DELINK\033[0m";
-elif [[ $DELINQUEENT == false ]]; then echo -e "\033[32m VOTING\033[0m"; 
+if   [[ $DELINQUEENT == true ]];  then echo -e "\033[31m DELINK\033[0m"$STATUS;
+elif [[ $DELINQUEENT == false ]]; then echo -e "\033[32m VOTING\033[0m"$STATUS; 
 else     echo -e "\033[31m unknown voting status $DELINQUEENT\033[0m";
 fi
