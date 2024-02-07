e77657e60a4cb317620ecf3b3b6587b5518badb7
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-02-07 18:39:35 +0300

    Update vote_on.sh

--- setup/vote_on.sh
+++ setup/vote_on.sh
@@ -16,15 +16,20 @@ if [ -z "$FORCE" ]; then # no 'force' flag, so waiting for 'delink' status
   done
 fi
 
-if [ -f ~/solana/ledger/tower-1_9-$PUB_KEY.bin ]; then 
-  TOWER_STATUS=' with existing tower'
-  solana-validator -l ~/solana/ledger set-identity --require-tower ~/solana/validator-keypair.json; 
-else
-  TOWER_STATUS=' without tower'
-  solana-validator -l ~/solana/ledger set-identity ~/solana/validator-keypair.json;
+if [ -f ~/solana/ledger/tower-1_9-$PUB_KEY.bin ]; 
+then  TOWER_STATUS=' with existing tower'; TOWER_FLAG="--require-tower"
+else  TOWER_STATUS=' without tower';       TOWER_FLAG=""
 fi
+
+command_output=$(solana-validator -l ~/solana/ledger set-identity $TOWER_FLAG ~/solana/validator-keypair.json 2>&1)
+command_exit_status=$?
+echo $command_output 
+if [ $command_exit_status -eq 0 ]; then   echo -e "\033[32m set validator-keypair successful \033[0m" 
+else                                      echo -e "\033[31m can not set validator-keypair \033[0m"
+fi
+
 ln -sfn ~/solana/validator-keypair.json ~/solana/validator_link.json
 # update telegraf
 sed -i "/^  hostname = /c\  hostname = \"$NAME\"" /etc/telegraf/telegraf.conf
 systemctl start telegraf
-echo -e "\033[31m vote ON\033[0m"$TOWER_STATUS
+echo -e "\033[32m vote ON\033[0m"$TOWER_STATUS
