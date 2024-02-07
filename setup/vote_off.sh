7df01de5a7b9fcf31f229279e8ca96f9dd3ef555
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-02-07 17:33:51 +0300

    Update vote_off.sh

--- setup/vote_off.sh
+++ setup/vote_off.sh
@@ -4,7 +4,15 @@ source $HOME/.bashrc
 if [ ! -f ~/solana/empty-validator.json ]; then 
 solana-keygen new -s --no-bip39-passphrase -o ~/solana/empty-validator.json
 fi
-solana-validator -l ~/solana/ledger set-identity ~/solana/empty-validator.json
+
 ln -sf ~/solana/empty-validator.json ~/solana/validator_link.json
+
+command_output=$(solana-validator -l ~/solana/ledger set-identity ~/solana/empty-validator.json 2>&1)
+command_exit_status=$?
+echo $command_output 
+if [ $command_exit_status -eq 0 ]; then   echo -e "\033[32m  set empty identity successful \033[0m" 
+else                                      echo -e "\033[31m  can not set empty identity \033[0m"
+fi
+
 systemctl stop telegraf
-echo -e "\033[32m vote OFF\033[0m"
+echo -e "\033[31m vote OFF\033[0m"
