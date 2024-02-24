83a6ee2dbdd332600ad52c8cb2e794dbc3ba6411
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-02-24 21:30:58 +0300

    Update node_set.sh

--- setup/node_set.sh
+++ setup/node_set.sh
@@ -29,14 +29,16 @@ echo 'export LEDGER=/mnt/disk2/ledger' >> $HOME/.bashrc
 solana config set --url https://api.mainnet-beta.solana.com --keypair ~/solana/validator-keypair.json
 cp ~/sol_git/Jito/solana.service ~/solana/solana.service
 sed -i "/^--allowed-validators /c\--allowed-validators $validator_key" ~/solana/jito-relayer.service
+systemctl daemon-reload
+systemctl restart jito-relayer.service
 echo -e "\033[31m set MAIN $NAME\033[0m"
 elif [[ $NODE == "test" ]]; then
 echo 'export LEDGER=~/solana/ledger' >> $HOME/.bashrc
 solana config set --url https://api.testnet.solana.com --keypair ~/solana/validator-keypair.json
 cp ~/sol_git/test/solana.service ~/solana/solana.service
+systemctl daemon-reload
 echo -e "\033[34m set test $NAME\033[0m"
 else
 echo -e "\033[31m Warning, unknown node type: $NODE \033[0m"
 fi
-systemctl daemon-reload
 ~/sol_git/setup/check.sh
