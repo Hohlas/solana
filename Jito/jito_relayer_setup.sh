3d18f62a0d69a410eb26fa02c76fa03547048d08
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-03-24 16:14:11 +0300

    Update jito_relayer_setup.sh

--- Jito/jito_relayer_setup.sh
+++ Jito/jito_relayer_setup.sh
@@ -5,7 +5,7 @@ apt install chrony -y
 cp ~/sol_git/Jito/jito-relayer.service ~/solana/jito-relayer.service
 ln -sf ~/solana/jito-relayer.service /etc/systemd/system
 RelayerKey=$(sed -n -e "/${NAME,,}_relayer/ s/^[^ ]* //p" ~/sol_git/Jito/relayer_address.txt)
-echo $RelayerKey
+echo 'relayer key '$RelayerKey
 sed -i "/^--allowed-validators /c\--allowed-validators $RelayerKey" ~/solana/jito-relayer.service
 systemctl restart chronyd.service
 # systemctl restart jito-relayer.service
