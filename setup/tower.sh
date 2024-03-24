bde09d85a98fa423821eb6ce1b98d6577a624db3
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-03-24 13:32:05 +0300

    Update tower.sh

--- setup/tower.sh
+++ setup/tower.sh
@@ -14,23 +14,21 @@ else echo -e '\033[31m - WARNING !!! no any *.ssh files in ~/keys - \033[0m'
 fi 
 
 # wait for window
-solana-validator -l ~/solana/ledger wait-for-restart-window --min-idle-time 10 --skip-new-snapshot-check
+# solana-validator -l ~/solana/ledger wait-for-restart-window --min-idle-time 10 --skip-new-snapshot-check
 
 # read current keys status
-empty=$(solana address -k ~/solana/empty-validator.json)
-link=$(solana address -k ~/solana/validator_link.json)
 validator=$(solana address -k ~/solana/validator-keypair.json)
 
 # get tower from Secondary server
 if [[ $DIR == 'from' ]]; then 
 echo -e "\033[31m get tower from\033[0m" $SERV; 
-read -p "are you ready? " RESP; if [ "$RESP" != "y" ]; then exit 1; fi
+#read -p "are you ready? " RESP; if [ "$RESP" != "y" ]; then exit 1; fi
 scp -P 2010 -i /root/keys/*.ssh $SERV:/root/solana/ledger/tower-1_9-$validator.bin /root/solana/ledger
 fi
 
 # send tower to Secondary server
 if [[ $DIR == 'to' ]]; then 
 echo -e "\033[32m send tower to\033[0m" $SERV; 
-read -p "are you ready? " RESP; if [ "$RESP" != "y" ]; then exit 1; fi
+#read -p "are you ready? " RESP; if [ "$RESP" != "y" ]; then exit 1; fi
 scp -P 2010 -i /root/keys/*.ssh /root/solana/ledger/tower-1_9-$validator.bin $SERV:/root/solana/ledger
 fi
