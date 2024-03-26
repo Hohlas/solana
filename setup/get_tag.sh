87bbceb1f5d9d3f8538606421b91b88e85294e72
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-03-26 15:06:33 +0300

    Update get_tag.sh

--- setup/get_tag.sh
+++ setup/get_tag.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$1" == "test" ]; then
+if [ "$1" == "main" ]; then
     REPO_URL="https://api.github.com/repos/jito-foundation/jito-solana/releases/latest"
     TAG=$(curl -sSL "$REPO_URL" | jq -r '.tag_name')
-elif [ "$1" == "main" ]; then
+elif [ "$1" == "test" ]; then
     REPO_URL="https://api.github.com/repos/solana-labs/solana/releases"
     TAG=$(curl -sSL "$REPO_URL" | jq -r '.[] | select(.name | startswith("Testnet")) | .tag_name' | head -n 1)
 else
