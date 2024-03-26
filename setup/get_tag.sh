a7bf2167decc79151a7a3ae6054fa981cba636d1
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-03-26 16:31:53 +0300

    Update get_tag.sh

--- setup/get_tag.sh
+++ setup/get_tag.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-if [ "$1" == "main" ]; then
+if [ -z "$1" ]; then
+	CUR_NODE=$NODE
+else
+	CUR_NODE=$1
+fi
+
+if [ "$CUR_NODE" == "main" ]; then
     REPO_URL="https://api.github.com/repos/jito-foundation/jito-solana/releases/latest"
     TAG=$(curl -sSL "$REPO_URL" | jq -r '.tag_name')
-elif [ "$1" == "test" ]; then
+elif [ "$CUR_NODE" == "test" ]; then
     REPO_URL="https://api.github.com/repos/solana-labs/solana/releases"
     TAG=$(curl -sSL "$REPO_URL" | jq -r '.[] | select(.name | startswith("Testnet")) | .tag_name' | head -n 1)
 else
-    echo "Usage: $0 [test|main]"
+    echo "NODE variable unknown $CUR_NODE"
 fi
 
 echo "$TAG"
