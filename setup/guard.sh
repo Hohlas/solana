fcc7aff592c7496b6bd83781c3394d3e8f39e449
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-03-31 12:53:45 +0300

    Update guard.sh

--- setup/guard.sh
+++ setup/guard.sh
@@ -12,7 +12,7 @@ SITES=("www.google.com" "www.bing.com")
 CONNECTION_LOSS_SCRIPT="$HOME/sol_git/setup/vote_off.sh"
 DISCONNECT_COUNTER=0
 SERV='root@'$(solana gossip | grep $PUB_KEY | awk '{print $1}')
-SERV_TYPE='SECONDARY'
+SERV_TYPE='Secondary'
 IP=$(echo "$SERV" | cut -d'@' -f2) # cut IP from root@IP
 #===
 BOT_TOKEN=5076252443:AAF1rtoCAReYVY8QyZcdXGmuUOrNVICllWU
@@ -87,7 +87,7 @@ CHECK_CONNECTION() { # self check connection every 5 seconds
 if [ "$CUR_IP" == "$IP" ]; then
   echo -e "\n solana voting on current PRIMARY  SERVER "
   date +"SELF_CHECK start  %b %e %H:%M:%S" >> ~/guard.log
-  SERV_TYPE='PRIMARY'
+  SERV_TYPE='Primary'
   # CHECK_CONNECTION_LOOP 
   until [ $DISCONNECT_COUNTER -ge 4 ]; do
     sleep 5
