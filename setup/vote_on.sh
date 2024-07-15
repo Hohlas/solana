598aa4e86826360fe1589fb9ea49e6c1860cda91
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-07-15 11:31:15 +0300

    Update vote_on.sh

--- setup/vote_on.sh
+++ setup/vote_on.sh
@@ -1,101 +1,32 @@
 #!/bin/bash
 # # #   Start Voting   # # # # # # # # # # # # # # # # # # # # #
 source $HOME/.bashrc
-PORT='2010'
-PUB_KEY=$(solana address -k ~/solana/validator-keypair.json) 
-SOL=$HOME/.local/share/solana/install/active_release/bin
 rpcURL=$(solana config get | grep "RPC URL" | awk '{print $3}')
-CUR_IP=$(wget -q -4 -O- http://icanhazip.com)
-GREY=$'\033[90m'; GREEN=$'\033[32m'; RED=$'\033[31m'
-SERV='root@'$(solana gossip | grep $PUB_KEY | awk '{print $1}')
-VOTING_IP=$(echo "$SERV" | cut -d'@' -f2) # cut IP from root@IP
+PUB_KEY=$(solana-keygen pubkey ~/solana/validator-keypair.json)
 
 FORCE=$1
 if [ -z "$FORCE" ]; then # no 'force' flag, so waiting for 'delink' status
-	Become_primary=0 # чтоб не срабатывало с первого раза
-	until [[ $Become_primary -ge 3 ]]; do
-		JSON=$(solana validators --url $rpcURL --output json-compact 2>/dev/null | jq '.validators[] | select(.identityPubkey == "'"${PUB_KEY}"'" )')
-		LastVote=$(echo "$JSON" | jq -r '.lastVote')
-		Delinquent=$(echo "$JSON" | jq -r '.delinquent')
-		if [[ $Delinquent == true ]]; then
-			let Become_primary=Become_primary+1
-		else 
-			Become_primary=0
-		fi
-		echo -ne " waiating node delink $(TZ=Europe/Moscow date +"%H:%M:%S") MSK,${CLR}    \r \033[0m"
-		sleep 5
-	done
-else # any symbols after 'vote_on' 
-	if [ "$CUR_IP" == "$VOTING_IP" ]; then # 
-		echo -e "$RED  node voiting on current server  \033[0m"
-		return
-	fi	
-	chmod 600 ~/keys/*.ssh
-	eval "$(ssh-agent -s)"  # Start ssh-agent in the background
-	ssh-add ~/keys/*.ssh # Add SSH private key to the ssh-agent
-
-	SERV='root@'$(solana gossip | grep $PUB_KEY | awk '{print $1}')
-	REMOTE_IP=$(echo "$SERV" | cut -d'@' -f2) # cut IP from root@IP
-	# create ssh alias for remote server
-	echo " 
-	Host REMOTE
-	HostName $REMOTE_IP
-	User root
-	Port $PORT
-	IdentityFile /root/keys/*.ssh
-	" > ~/.ssh/config
-
-	# check SSH connection with primary node server
-	command_output=$(ssh REMOTE 'echo "SSH connection succesful" > ~/check_ssh')
-	command_exit_status=$?
-	if [ $command_exit_status -ne  0 ]; then
-		echo -e "$RED SSH connection not available  \033[0m"
-		return
-	fi
-	scp -P $PORT -i /root/keys/*.ssh root@$REMOTE_IP:~/check_ssh ~/
-	ssh REMOTE rm ~/check_ssh
-	echo -e "$GREEN$(cat ~/check_ssh)\033[0m"
-	rm ~/check_ssh
-	
-	command_output=$(ssh -o ConnectTimeout=5 REMOTE ln -sf ~/solana/empty-validator.json ~/solana/validator_link.json 2>&1)
-	command_exit_status=$?
-	if [ $command_exit_status -eq 0 ]; then
-		echo -e " change validator link on REMOTE server $GREEN successful \033[0m" 
-  	else
-   		echo -e "$RED can't change validator link on REMOTE server \033[0m"
-     		echo $command_output
-		return
-	fi
-	
-	command_output=$(ssh -o ConnectTimeout=5 REMOTE $SOL/solana-validator -l ~/solana/ledger set-identity ~/solana/empty-validator.json 2>&1)
-	command_exit_status=$? 
-	if [ $command_exit_status -eq 0 ]; then
-		echo -e " set empty identity on REMOTE server $GREEN successful \033[0m" 
-	else
-		echo -e "$RED  can't set empty identity on REMOTE server \033[0m"
-  		echo $command_output
-		return
-	fi
- 
-	echo "  move tower from REMOTE to LOCAL "
-	timeout 5 scp -P $PORT -i /root/keys/*.ssh $SERV:/root/solana/ledger/tower-1_9-$PUB_KEY.bin /root/solana/ledger
-	echo "  stop telegraf on REMOTE server"
-	ssh -o ConnectTimeout=5 REMOTE systemctl stop telegraf
-	# echo "  stop jito-relayer on REMOTE server"
-	
+  Delinquent=false
+  until [[ $Delinquent == true ]]; do
+    JSON=$(solana validators --url $rpcURL --output json-compact 2>/dev/null | jq '.validators[] | select(.identityPubkey == "'"${PUB_KEY}"'" )')
+    LastVote=$(echo "$JSON" | jq -r '.lastVote')
+    Delinquent=$(echo "$JSON" | jq -r '.delinquent')
+    echo -ne "Looking for "$PUB_KEY". LastVote="$LastVote" \r"
+    sleep 3
+  done
 fi
 
-if [ -f ~/solana/ledger/tower-1_9-$PUB_KEY.bin ]; then  
-	TOWER_STATUS=' with existing tower'; TOWER_FLAG="--require-tower"
-else  
-	TOWER_STATUS=' without tower';       TOWER_FLAG=""
+if [ -f ~/solana/ledger/tower-1_9-$PUB_KEY.bin ]; 
+then  TOWER_STATUS=' with existing tower'; TOWER_FLAG="--require-tower"
+else  TOWER_STATUS=' without tower';       TOWER_FLAG=""
 fi
+
 command_output=$(solana-validator -l ~/solana/ledger set-identity $TOWER_FLAG ~/solana/validator-keypair.json 2>&1)
 command_exit_status=$?
 echo $command_output 
 if [ $command_exit_status -eq 0 ]; 
-then echo -e "$GREEN set validator-keypair successful \033[0m" 
-else echo -e "$RED can't set validator-keypair \033[0m"
+then echo -e "\033[32m set validator-keypair successful \033[0m" 
+else echo -e "\033[31m can not set validator-keypair \033[0m"
 fi
 
 # ln -sfn ~/solana/validator-keypair.json ~/solana/validator_link.json
