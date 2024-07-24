63b0366f49c3259d257baa9d8da1509bab7e1da2
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-07-24 10:55:05 +0300

    Update update.sh
    add links to solana/bin

--- setup/update.sh
+++ setup/update.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 solana-install init $TAG
 solana --version
+# create simlinks  agave -> solana
+cd "/root/.local/share/solana/install/active_release/bin/" || exit
+for file in agave-*; do # Перебираем все файлы, начинающиеся с "agave-"
+    if [ -f "$file" ]; then # файл существует ли 
+        ln -sf "$file" "${file/agave-/solana-}" # Создаем символическую ссылку
+        echo "create link for $file"
+    fi
+done
