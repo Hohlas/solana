5c906d9d1c7c4b4c47d0bdad200737144d952023
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-07-24 10:37:52 +0300

    Update get_tag.sh
    change github to anza-xyz/agave

--- setup/get_tag.sh
+++ setup/get_tag.sh
@@ -10,8 +10,8 @@ if [ "$CUR_NODE" == "main" ]; then
     REPO_URL="https://api.github.com/repos/jito-foundation/jito-solana/releases/latest"
     TAG=$(curl -sSL "$REPO_URL" | jq -r '.tag_name')
 elif [ "$CUR_NODE" == "test" ]; then
-    REPO_URL="https://api.github.com/repos/solana-labs/solana/releases"
-    TAG=$(curl -sSL "$REPO_URL" | jq -r '.[] | select(.name | startswith("Testnet")) | .tag_name' | head -n 1)
+    REPO_URL="https://api.github.com/repos/anza-xyz/agave/releases"
+    TAG=$(curl -sSL "$REPO_URL" | jq -r '.[0].tag_name')
 else
     echo "NODE variable unknown $CUR_NODE"
 fi
