83c8d7a58d8e0cc42575c17523c19b2508245415
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-08-01 07:18:10 +0300

    Update finder.sh

--- setup/finder.sh
+++ setup/finder.sh
@@ -6,7 +6,7 @@ cd ~/solana-snapshot-finder \
 
 systemctl daemon-reload
 
-if [[ $NODE == "main" ]]; then
+if [[ $NODE == "MAIN" ]]; then
 python3 snapshot-finder.py --snapshot_path /mnt/disk2/ledger --num_of_retries 10 --measurement_time 10 --min_download_speed 40 --max_snapshot_age 500 --max_latency 500 --with_private_rpc --sort_order latency -r https://api.mainnet-beta.solana.com && \
 systemctl restart solana && tail -f ~/solana/solana.log
 elif [[ $NODE == "test" ]]; then
