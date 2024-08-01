1b4ebf6e7855560d30bc9dae23c0f4a1fa50f9f4
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-08-01 15:55:26 +0300

    Update finder.sh

--- setup/finder.sh
+++ setup/finder.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+rpcURL=$(solana config get | grep "RPC URL" | awk '{print $3}')
 
-cd ~/solana-snapshot-finder \
-&& python3 -m venv venv \
-&& source ./venv/bin/activate
-
+cd ~/solana-snapshot-finder && python3 -m venv venv && source ./venv/bin/activate
 systemctl daemon-reload
 
-if [[ $NODE == "MAIN" ]]; then
+if [[ $rpcURL = https://api.mainnet-beta.solana.com ]]; then
 python3 snapshot-finder.py --snapshot_path /mnt/disk2/ledger --num_of_retries 10 --measurement_time 10 --min_download_speed 40 --max_snapshot_age 500 --max_latency 500 --with_private_rpc --sort_order latency -r https://api.mainnet-beta.solana.com && \
 systemctl restart solana && tail -f ~/solana/solana.log
-elif [[ $NODE == "test" ]]; then
+elif [[ $rpcURL = https://api.testnet.solana.com ]]; then
 python3 snapshot-finder.py --snapshot_path $HOME/solana/ledger --num_of_retries 10 --measurement_time 10 --min_download_speed 50 --max_snapshot_age 500 --with_private_rpc --sort_order latency -r https://api.testnet.solana.com && \
 systemctl daemon-reload && systemctl restart solana
 tail -f ~/solana/solana.log
