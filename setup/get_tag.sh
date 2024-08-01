b502b5981c8f9f3c6a8071b03fdf009611ee2bb5
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-08-01 15:59:50 +0300

    Update get_tag.sh

--- setup/get_tag.sh
+++ setup/get_tag.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-	CUR_NODE=$NODE
+rpcURL=$(solana config get | grep "RPC URL" | awk '{print $3}')
+
+if [ $rpcURL = https://api.mainnet-beta.solana.com ]; then
+	CUR_NET="MainNet"
+    	REPO_URL="https://api.github.com/repos/jito-foundation/jito-solana/releases/latest"
+    	TAG=$(curl -sSL "$REPO_URL" | jq -r '.tag_name')
+elif [ $rpcURL = https://api.testnet.solana.com ]; then
+    	CUR_NET="TestNet"
+	REPO_URL="https://api.github.com/repos/anza-xyz/agave/releases"
+    	TAG=$(curl -sSL "$REPO_URL" | jq -r '.[0].tag_name')
 else
-	CUR_NODE=$1
+    	echo "NODE variable unknown $CUR_NODE"
 fi
 
-if [ "$CUR_NODE" == "main" ]; then
-    REPO_URL="https://api.github.com/repos/jito-foundation/jito-solana/releases/latest"
-    TAG=$(curl -sSL "$REPO_URL" | jq -r '.tag_name')
-elif [ "$CUR_NODE" == "test" ]; then
-    REPO_URL="https://api.github.com/repos/anza-xyz/agave/releases"
-    TAG=$(curl -sSL "$REPO_URL" | jq -r '.[0].tag_name')
-else
-    echo "NODE variable unknown $CUR_NODE"
-fi
-
-echo " TAG=$TAG"
+echo "$CUR_NET TAG=$TAG"
