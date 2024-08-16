2ed3ff860d869497c101d090b54f38f9d6db708c
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-08-16 21:31:10 +0300

    Update guard.sh

--- setup/guard.sh
+++ setup/guard.sh
@@ -100,7 +100,7 @@ SSH(){
 			echo "$(TIME) remote server $REMOTE_IP ping OK" | tee -a ~/guard.log
 		else
 			echo "$(TIME) remote server $REMOTE_IP did not ping" | tee -a ~/guard.log
-			if ping -c 3 -W 3 "$www.google.com" > /dev/null 2>&1; then
+			if ping -c 3 -W 3 "www.google.com" > /dev/null 2>&1; then
 				echo "$(TIME) google ping OK" | tee -a ~/guard.log
 			else
 				echo "$(TIME) google did not ping too" | tee -a ~/guard.log
