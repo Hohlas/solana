fab592511e565596daf0867f66b548ad97846601
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-08-16 13:52:53 +0300

    Update bot_script_setup.sh
    edit  cron

--- telegram_bot/bot_script_setup.sh
+++ telegram_bot/bot_script_setup.sh
@@ -9,13 +9,13 @@ sudo apt update && sudo apt upgrade -y && sudo apt install jq cron iputils-ping 
 #echo 'export PATH='$PATH >> $HOME/.bashrc
 #source $HOME/.bashrc
 echo "download telegram scripts"
-curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_main.sh > ~/solana/watch_main.sh && chmod +x ~/solana/watch_main.sh
-curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_test.sh > ~/solana/watch_test.sh && chmod +x ~/solana/watch_test.sh
+#curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_main.sh > ~/solana/watch_main.sh && chmod +x ~/solana/watch_main.sh
+#curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/watch_test.sh > ~/solana/watch_test.sh && chmod +x ~/solana/watch_test.sh
 echo "create cron task"
 crontab -l >$HOME/tmp.txt  # copy crontab to tmp.txt
 echo "MAILTO=''
-5,15,25,35,45,55 * * * * /root/solana/watch_test.sh
-6,16,26,36,46,56 * * * * /root/solana/watch_main.sh
+5,15,25,35,45,55 * * * * /root/sol_git/telegram_bot/watch_test.sh
+6,16,26,36,46,56 * * * * /root/sol_git/telegram_bot/watch_main.sh
 " >> ~/tmp.txt     # add 
 crontab $HOME/tmp.txt  # copy tmp.txt to crontab
 sudo rm $HOME/tmp.txt  # remove tmp file
