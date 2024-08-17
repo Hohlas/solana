45abc68c472fd29d091e9c4e560493fdcf65f646
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-08-17 15:01:46 +0300

    Update node_setup.sh
    remove SWAP setup

--- setup/node_setup.sh
+++ setup/node_setup.sh
@@ -17,28 +17,6 @@ else
     echo "~/keys exist"
 fi
 
-echo -e '\n\e[42m create SWAP \e[0m\n'	
-SWAP_SIZE=300 # required SWAP size
-MIN_DIFFERENCE=1
-CURRENT_SWAP_SIZE=$(free -g | awk '/^Swap:/ {print $2}')
-ADDITIONAL_SWAP=$((SWAP_SIZE - CURRENT_SWAP_SIZE))
-if [ "$ADDITIONAL_SWAP" -gt "$MIN_DIFFERENCE" ]; then
-    echo -e " current SWAP size\033[32m ${CURRENT_SWAP_SIZE}G \033[0m"
-	echo -e " create additional SWAP\033[32m ${ADDITIONAL_SWAP}G \033[0m"
-    command_output=$(fallocate -l ${ADDITIONAL_SWAP}G /swapfile2) 
-	command_exit_status=$?
-	if [ $command_exit_status -ne 0 ]; then
-		echo -e "\033[31m can't create swapfile2 \033[0m"
-	else
-		chmod 600 /swapfile2
-		mkswap /swapfile2
-		swapon /swapfile2
-		echo "/swapfile2 none swap sw 0 0" | sudo tee -a /etc/fstab
-	fi
-else
-    echo -e " current SWAP size\033[32m $CURRENT_SWAP_SIZE\033[0m enough "
-fi
-
 echo -e '\n\e[42m change swappiness \e[0m\n'
 sudo sysctl vm.swappiness=10  # change current SWAPPINESS
 echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf # change after reboot SWAPPINESS
