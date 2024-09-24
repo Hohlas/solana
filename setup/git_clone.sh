3954c000968e2f455082d8e282a01e9fb4027dec
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-09-24 18:01:43 +0300

    Update git_clone.sh

--- setup/git_clone.sh
+++ setup/git_clone.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 if [ -d ~/sol_git ]; then 
-cd ~/sol_git; 
-git fetch origin; # get last updates from git
-#git diff # просмотреть изменения, 
-#git merge # применить изменения к локальному репу
-git reset --hard origin/main # сбросить локальную ветку до последнего коммита из git
-else git clone https://github.com/Hohlas/solana.git ~/sol_git
+  cd ~/sol_git; 
+  git fetch origin; # get last updates from git
+  #git diff # просмотреть изменения, 
+  #git merge # применить изменения к локальному репу
+  git reset --hard origin/main # сбросить локальную ветку до последнего коммита из git
+else 
+  git clone https://github.com/Hohlas/solana.git ~/sol_git
 fi
 chmod +x ~/sol_git/setup/*.sh
 chmod +x ~/sol_git/telegram_bot/watch_test.sh
