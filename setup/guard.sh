792e7e3de13acce2fc6b04e897d36c41e1c9f39a
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-10-24 19:16:09 +0300

    Update guard.sh

--- setup/guard.sh
+++ setup/guard.sh
@@ -132,7 +132,7 @@ RPC_REQUEST() {
     	else 
       		CLR1=$YELLOW; CLR2=$GREEN;
     	fi 
-    	echo -e "$(TIME) Warning! Different answers $BLUE$percentage%$CLEAR: RPC1=$CLR1$REQUEST1$CLEAR, RPC2=$CLR2$REQUEST2$CLEAR.     " | tee -a ~/guard.log	
+    	echo -e "$(TIME) Warning! Different answers $BLUE$percentage%$CLEAR: RPC1=[$CLR1$REQUEST1$CLEAR] RPC2=[$CLR2$REQUEST2$CLEAR]     " | tee -a ~/guard.log	
   		if [[ $percentage -lt 70 ]]; then 
 			REQUEST_ANSWER="";
    			echo -e "$(TIME) Error: REQUEST_ANSWER not so correct, disable it" | tee -a ~/guard.log
