086893e3e8e16eadf667c20b338f01e1f2df135f
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-11-13 16:41:28 +0300

    Update patch.sh

--- Jito/patch.sh
+++ Jito/patch.sh
@@ -11,11 +11,12 @@ apt install libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make l
 # TAG=v2.0.15-jito
 if [ -d ~/jito ]; then 
   cd ~/jito; 
-  git pull origin master
+  git fetch origin; 
+  git reset --hard origin/master # сбросить локальную ветку до последнего коммита из git
 else 
   git clone https://github.com/jito-foundation/jito-solana.git ~/jito
 fi
-git fetch --tags
+git fetch --tags # для загрузки всех тегов из удаленного репозитория
 git checkout tags/$TAG
 curl -o ~/jito/core/src/consensus.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/consensus.rs
 curl -o ~/jito/core/src/consensus/progress_map.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/consensus/progress_map.rs
@@ -24,6 +25,7 @@ curl -o ~/jito/core/src/vote_simulator.rs https://github.com/bji/solana/blob/915
 curl -o ~/jito/programs/vote/src/vote_state/mod.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/programs/vote/src/vote_state/mod.rs
 curl -o ~/jito/sdk/program/src/vote/state/mod.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/sdk/program/src/vote/state/mod.rs
 
+$ ./cargo build
 
 
 
