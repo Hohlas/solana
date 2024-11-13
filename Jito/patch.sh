337813cd8ed57bc39012e2b54bc1d78011a32d64
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-11-13 16:33:38 +0300

    Update patch.sh

--- Jito/patch.sh
+++ Jito/patch.sh
@@ -8,18 +8,21 @@ rustup update
 apt update
 apt install libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make libprotobuf-dev protobuf-compiler -y
 
-if [ -d ~/agave ]; then 
-  cd ~/agave; 
-  git fetch origin; # get last updates from git
-  #git diff # просмотреть изменения, 
-  #git merge # применить изменения к локальному репу
-  git reset --hard origin/main # сбросить локальную ветку до последнего коммита из git
+# TAG=v2.0.15-jito
+if [ -d ~/jito ]; then 
+  cd ~/jito; 
+  git pull origin master
 else 
-  git clone https://github.com/anza-xyz/agave.git ~/agave
+  git clone https://github.com/jito-foundation/jito-solana.git ~/jito
 fi
-
-
-
+git fetch --tags
+git checkout tags/$TAG
+curl -o ~/jito/core/src/consensus.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/consensus.rs
+curl -o ~/jito/core/src/consensus/progress_map.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/consensus/progress_map.rs
+curl -o ~/jito/core/src/replay_stage.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/replay_stage.rs
+curl -o ~/jito/core/src/vote_simulator.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/vote_simulator.rs
+curl -o ~/jito/programs/vote/src/vote_state/mod.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/programs/vote/src/vote_state/mod.rs
+curl -o ~/jito/sdk/program/src/vote/state/mod.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/sdk/program/src/vote/state/mod.rs
 
 
 
