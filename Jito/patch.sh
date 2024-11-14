a767fd87025265b58fd5b2d6e4087aba9e38fc37
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-11-14 10:28:55 +0300

    Update patch.sh

--- Jito/patch.sh
+++ Jito/patch.sh
@@ -8,26 +8,29 @@ rustup component add rustfmt
 apt update
 apt install libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make libprotobuf-dev protobuf-compiler -y
 
-# TAG=v2.0.15-jito
+# export TAG=v2.0.15-jito
 if [ -d ~/jito-solana ]; then 
   cd ~/jito-solana; 
   git fetch origin; 
   git reset --hard origin/master # сбросить локальную ветку до последнего коммита из git
 else 
-  git clone https://github.com/jito-foundation/jito-solana.git
+  git clone https://github.com/jito-foundation/jito-solana.git --recurse-submodules
   cd jito-solana
 fi
+
 git fetch --tags # для загрузки всех тегов из удаленного репозитория
 git checkout tags/$TAG
-curl -o ~/jito/core/src/consensus.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/consensus.rs
-curl -o ~/jito/core/src/consensus/progress_map.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/consensus/progress_map.rs
-curl -o ~/jito/core/src/replay_stage.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/replay_stage.rs
-curl -o ~/jito/core/src/vote_simulator.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/vote_simulator.rs
-curl -o ~/jito/programs/vote/src/vote_state/mod.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/programs/vote/src/vote_state/mod.rs
-curl -o ~/jito/sdk/program/src/vote/state/mod.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/sdk/program/src/vote/state/mod.rs
+git submodule update --init --recursive
 
-./cargo build
+curl -o ~/jito-solana/core/src/consensus.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/consensus.rs
+curl -o ~/jito-solana/core/src/consensus/progress_map.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/consensus/progress_map.rs
+curl -o ~/jito-solana/core/src/replay_stage.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/replay_stage.rs
+curl -o ~/jito-solana/core/src/vote_simulator.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/vote_simulator.rs
+curl -o ~/jito-solana/programs/vote/src/vote_state/mod.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/programs/vote/src/vote_state/mod.rs
+curl -o ~/jito-solana/sdk/program/src/vote/state/mod.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/sdk/program/src/vote/state/mod.rs
 
+# ./cargo build
+CI_COMMIT=$(git rev-parse HEAD) scripts/cargo-install-all.sh --validator-only ~/.local/share/solana/install/releases/"$TAG"
 
 
 
