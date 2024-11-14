85560abca78932b28cf071fdea96c24a2a21ff7c
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-11-14 10:10:43 +0300

    Update patch.sh

--- Jito/patch.sh
+++ Jito/patch.sh
@@ -3,7 +3,7 @@
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 rustup component add rustfmt
-rustup update
+. "$HOME/.cargo/env"
 
 apt update
 apt install libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make libprotobuf-dev protobuf-compiler -y
@@ -20,11 +20,11 @@ fi
 git fetch --tags # для загрузки всех тегов из удаленного репозитория
 git checkout tags/$TAG
 curl -o ~/jito/core/src/consensus.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/consensus.rs
-curl -o ~/jito/core/src/consensus/progress_map.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/consensus/progress_map.rs
-curl -o ~/jito/core/src/replay_stage.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/replay_stage.rs
-curl -o ~/jito/core/src/vote_simulator.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/vote_simulator.rs
-curl -o ~/jito/programs/vote/src/vote_state/mod.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/programs/vote/src/vote_state/mod.rs
-curl -o ~/jito/sdk/program/src/vote/state/mod.rs https://github.com/bji/solana/blob/915909fc8539d4df7cc11ba14226ad6247c53cdb/sdk/program/src/vote/state/mod.rs
+curl -o ~/jito/core/src/consensus/progress_map.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/consensus/progress_map.rs
+curl -o ~/jito/core/src/replay_stage.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/replay_stage.rs
+curl -o ~/jito/core/src/vote_simulator.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/core/src/vote_simulator.rs
+curl -o ~/jito/programs/vote/src/vote_state/mod.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/programs/vote/src/vote_state/mod.rs
+curl -o ~/jito/sdk/program/src/vote/state/mod.rs https://raw.githubusercontent.com/bji/solana/915909fc8539d4df7cc11ba14226ad6247c53cdb/sdk/program/src/vote/state/mod.rs
 
 ./cargo build
 
