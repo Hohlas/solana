dd588891d790976301a8531ddcd050601d4fc624
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-12-12 08:36:20 +0300

    Update node_reset.sh

--- setup/node_reset.sh
+++ setup/node_reset.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # # #   ReSet Node
 read -p "are you ready to RESET solana node? " RESP; if [ "$RESP" != "y" ]; then exit 1; fi
-echo "Stop solana.service and delete Ledger"
+echo "Stop solana.service"
 systemctl stop solana
-rm -rf ~/solana/ledger/* && echo "delete ledger"
+rm -rf ~/solana/ledger/* && echo "delete ledger/*"
 rm -rf /mnt/disk1/* && echo "delete disk1/*"
 rm -rf /mnt/disk2/* && echo "delete disk2/*"
 rm -rf /mnt/disk3/* && echo "delete disk3/*"
