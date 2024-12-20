9643d430d3202253fc248707fce77bf70bf16e49
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-12-20 19:10:19 +0300

    Update trim2cron.sh

--- setup/trim2cron.sh
+++ setup/trim2cron.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 crontab -l >$HOME/tmp.txt  # copy crontab to tmp.txt
-echo "MAILTO=''  # to avoid error: 'No MTA installed, discarding output'. 
-*/10 * * * * ~/.local/share/solana/install/active_release/bin/solana-validator --ledger ~/solana/ledger wait-for-restart-window --skip-new-snapshot-check --max-delinquent-stake 50 --min-idle-time 2 && (date +"  %b %e %H:%M:%S" && sudo fstrim -av)" >> ~/trim.log 2>&1
+echo "MAILTO=''  # to avoid error: 'No MTA installed, discarding output'. " >> ~/tmp.txt
+echo "
+*/10 * * * * ~/.local/share/solana/install/active_release/bin/solana-validator --ledger ~/solana/ledger wait-for-restart-window --skip-new-snapshot-check --max-delinquent-stake 50 --min-idle-time 2 && (date +'  %b %e %H:%M:%S' && sudo fstrim -av) >> ~/trim.log 2>&1" >> ~/tmp.txt
 crontab $HOME/tmp.txt  # copy tmp.txt to crontab
 sudo rm $HOME/tmp.txt  # remove tmp file
 sudo systemctl restart cron
