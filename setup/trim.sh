ae365a79092e94e52102090adcffdeb546592150
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-12-20 19:36:55 +0300

    Update trim.sh

--- setup/trim.sh
+++ setup/trim.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ~/.local/share/solana/install/active_release/bin/solana-validator --ledger ~/solana/ledger wait-for-restart-window --skip-new-snapshot-check --max-delinquent-stake 90 --min-idle-time 5 && (date +"  %b %e %H:%M:%S" && sudo fstrim -av) >> ~/trim.log 2>&1
+sudo ~/.local/share/solana/install/active_release/bin/solana-validator --ledger ~/solana/ledger wait-for-restart-window --skip-new-snapshot-check --max-delinquent-stake 50 --min-idle-time 2 && (date +"  %b %e %H:%M:%S" && sudo fstrim -av) >> ~/trim.log 2>&1
