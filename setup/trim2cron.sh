968cf12fdc9a3ebac68ce8d0621ff96d1fe67b22
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-12-20 19:42:27 +0300

    Update trim2cron.sh

--- setup/trim2cron.sh
+++ setup/trim2cron.sh
@@ -8,6 +8,7 @@ sudo crontab -l > "$TMP_FILE" 2>/dev/null || echo "" > "$TMP_FILE"
 
 # Добавляем MAILTO и новую задачу в временный файл
 echo "MAILTO=''" >> "$TMP_FILE"
+#echo "*/10 * * * * $HOME/sol_git/setup/trim.sh" >> "$TMP_FILE"
 echo "*/10 * * * * /home/yourusername/.local/share/solana/install/active_release/bin/solana-validator --ledger /home/yourusername/solana/ledger wait-for-restart-window --skip-new-snapshot-check --max-delinquent-stake 50 --min-idle-time 2 && (date '+%b %e %H:%M:%S' && sudo fstrim -av) >> /home/yourusername/trim.log 2>&1" >> "$TMP_FILE"
 
 # Копируем временный файл обратно в crontab для root
