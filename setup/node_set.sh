fa66d494b94fc16c14cafd6355fd7f4692187fa2
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-12-24 10:49:29 +0300

    Update node_set.sh

--- setup/node_set.sh
+++ setup/node_set.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 echo -e '\n\e[42m Node Set \e[0m\n'
-# update from git
-if [ -d ~/sol_git ]; then 
-cd ~/sol_git; 
-git fetch origin; # get last updates from git
-git reset --hard origin/main # сбросить локальную ветку до последнего коммита из git
-else 
-cd; git clone https://github.com/Hohlas/solana.git ~/sol_git
-fi
-chmod +x ~/sol_git/setup/*.sh
-chmod +x ~/sol_git/telegram_bot/watch_test.sh
-chmod +x ~/sol_git/telegram_bot/watch_main.sh
-# ln -sf ~/sol_git/telegram_bot/tg_bot_token ~/keys/tg_bot_token
-# curl https://raw.githubusercontent.com/Hohlas/solana/main/telegram_bot/tg_bot_token > ~/keys/tg_bot_token
 
 # update .bashrc, key links, grafana name
 ln -sf ~/keys/${NAME,,}_${NODE,,}_vote.json ~/solana/vote.json
@@ -28,24 +15,22 @@ echo 'export validator_key='$(solana address -k ~/solana/validator-keypair.json)
 echo 'export vote_account='$(solana address -k ~/solana/vote.json) >> $HOME/.bashrc
 source $HOME/.bashrc
 if [ ! -f ~/solana/empty-validator.json ]; then 
-solana-keygen new -s --no-bip39-passphrase -o ~/solana/empty-validator.json
+    echo "create empty-validator.json"
+    solana-keygen new -s --no-bip39-passphrase -o ~/solana/empty-validator.json
 fi
-# ln -sf ~/solana/empty-validator.json ~/solana/validator_link.json
-echo "download guard.cfg" # from buka
-# curl -o $HOME/guard.cfg https://raw.githubusercontent.com/Hohlas/solana/main/setup/cfg/${NAME,,}.cfg
 
 # update services and network url
 if [[ $NODE == "main" ]]; then
-solana config set --url https://api.mainnet-beta.solana.com --keypair ~/solana/validator-keypair.json
-cp ~/sol_git/Jito/solana.service ~/solana/solana.service
-sed -i "/^--allowed-validators /c\--allowed-validators $validator_key" ~/solana/jito-relayer.service
-echo -e "\033[31m set MAIN $NAME\033[0m"
+    solana config set --url https://api.mainnet-beta.solana.com --keypair ~/solana/validator-keypair.json
+    cp ~/sol_git/Jito/solana.service ~/solana/solana.service
+    sed -i "/^--allowed-validators /c\--allowed-validators $validator_key" ~/solana/jito-relayer.service
+    echo -e "\033[31m set MAIN $NAME\033[0m"
 elif [[ $NODE == "test" ]]; then
-solana config set --url https://api.testnet.solana.com --keypair ~/solana/validator-keypair.json
-cp ~/sol_git/test/solana.service ~/solana/solana.service
-echo -e "\033[34m set test $NAME\033[0m"
+    solana config set --url https://api.testnet.solana.com --keypair ~/solana/validator-keypair.json
+    cp ~/sol_git/test/solana.service ~/solana/solana.service
+    echo -e "\033[34m set test $NAME\033[0m"
 else
-echo -e "\033[31m Warning, unknown node type: $NODE \033[0m"
+    echo -e "\033[31m Warning, unknown node type: $NODE \033[0m"
 fi
 systemctl daemon-reload
 ~/sol_git/setup/check.sh
