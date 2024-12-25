29d44bfdade2a05046c6a587b1e78d6cabe51238
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-12-25 11:08:29 +0300

    Update node_reset.sh

--- setup/node_reset.sh
+++ setup/node_reset.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 # # #   ReSet Node
+SOLANA_SERVICE="$HOME/solana/solana.service"
 read -p "are you ready to RESET solana node? " RESP; if [ "$RESP" != "y" ]; then exit 1; fi
 echo "Stop solana.service"
 systemctl stop solana
+
+LEDGER_PATH=$(grep -oP '(?<=--ledger\s).*' "$SOLANA_SERVICE" | tr -d '\\')
+if [[ -z "$LEDGER_PATH" ]]; then
+    echo "remoove $LEDGER_PATH "
+    rm -r $LEDGER_PATH/*
+fi    
+
+
+echo -e "snapshot path =\033[32m $SNAPSHOT_PATH \033[0m "
+
+
+
+
 rm -rf ~/solana/ledger/* && echo "delete ledger/*"
 rm -rf /mnt/disk1/* && echo "delete disk1/*"
 rm -rf /mnt/disk2/* && echo "delete disk2/*"
