423507794a5824d7fdc06520c062008c5fc816f4
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-12-27 21:13:43 +0300

    Update firewall.sh

--- setup/firewall.sh
+++ setup/firewall.sh
@@ -447,17 +447,6 @@ DDoS_LIST_FILE="$HOME/DDOS_LIST.txt"
 DDOS_SEARCH() {
     # Получаем список IP-адресов с помощью ss, игнорируя заголовки
     #iftop -i "$INTERFACE" -t -s 10 | grep -oP '(\d{1,3}\.){3}\d{1,3}' | sort -u > "$OUTPUT_FILE"
-    ss -tnp | awk 'NR > 1 {print $5, $6}' | cut -d: -f1 | sort -u > "$OUTPUT_FILE"
-
-    # echo "Список IP-адресов сохранен в файле $OUTPUT_FILE"
-
-    # Проверка каждого IP-адреса на наличие в списке валидаторов
-    
-
-
-DDOS_SEARCH() {
-    # Получаем список IP-адресов с помощью ss, игнорируя заголовки
-    #iftop -i "$INTERFACE" -t -s 10 | grep -oP '(\d{1,3}\.){3}\d{1,3}' | sort -u > "$OUTPUT_FILE"
     ss -tnp | awk 'NR > 1 {print $5}' | cut -d: -f1 | sort -u > "$OUTPUT_FILE"
 
     # echo "Список IP-адресов сохранен в файле $OUTPUT_FILE"
@@ -475,7 +464,6 @@ DDOS_SEARCH() {
     done < "$OUTPUT_FILE"
 }
 
-
 # Основной цикл
 while true; do
 	RPC_REQUEST "IP"
@@ -484,9 +472,3 @@ while true; do
 done
 
 
-
-while true  ###  main cycle   #################################################
-do
-	echo "h"
-	sleep 2
-done
