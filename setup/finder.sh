d6fa42f5095854006d1bd15af100d8984d7dfb16
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-12-30 07:19:40 +0300

    Update finder.sh
    RESTART

--- setup/finder.sh
+++ setup/finder.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+read -p " Do You need to restart node after snapshot downloading? (y/n)" RESTART; 
+
 LEDGER=$HOME/solana/ledger
 SOLANA_SERVICE="$HOME/solana/solana.service"
 rpcURL=$(solana config get | grep "RPC URL" | awk '{print $3}')
@@ -14,10 +16,15 @@ echo -e "snapshot path =\033[32m $SNAPSHOT_PATH \033[0m "
 
 if [[ $rpcURL = https://api.mainnet-beta.solana.com ]]; then
     echo -e " download snapshot for\033[32m MainNet \033[0m"
-    python3 snapshot-finder.py --snapshot_path $SNAPSHOT_PATH --num_of_retries 10 --measurement_time 10 --min_download_speed 40 --max_snapshot_age 500 --max_latency 500 --with_private_rpc --sort_order latency -r https://api.mainnet-beta.solana.com && systemctl restart solana && tail -f ~/solana/solana.log
+    python3 snapshot-finder.py --snapshot_path $SNAPSHOT_PATH --num_of_retries 10 --measurement_time 10 --min_download_speed 40 --max_snapshot_age 500 --max_latency 500 --with_private_rpc --sort_order latency -r https://api.mainnet-beta.solana.com
 elif [[ $rpcURL = https://api.testnet.solana.com ]]; then
     echo -e " download snapshot for\033[32m TestNet \033[0m"
-    python3 snapshot-finder.py --snapshot_path $SNAPSHOT_PATH --num_of_retries 10 --measurement_time 10 --min_download_speed 50 --max_snapshot_age 500 --with_private_rpc --sort_order latency -r https://api.testnet.solana.com && systemctl restart solana && tail -f ~/solana/solana.log
+    python3 snapshot-finder.py --snapshot_path $SNAPSHOT_PATH --num_of_retries 10 --measurement_time 10 --min_download_speed 50 --max_snapshot_age 500 --with_private_rpc --sort_order latency -r https://api.testnet.solana.com
 else
     echo -e "\033[31m Warning, unknown node type: $NODE \033[0m"
+    return
 fi
+if [[ "$RESTART" == "y" ]]; then 
+    systemctl restart solana && tail -f ~/solana/solana.log
+fi    
+        # add snapshots
