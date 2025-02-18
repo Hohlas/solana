6c00a4a89fe52d1d30eecdb4f94266264ebe011f
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-02-18 08:48:45 +0300

    Update check.sh

--- setup/check.sh
+++ setup/check.sh
@@ -53,8 +53,8 @@ else TME_CLR=$GREEN; fi
 echo " == SOLANA CHECK $CHECK_VER"
 echo -e " $BLUE$NODE.$NAME $YELLOW$version $client $CLEAR"
 echo -e " next:$TME_CLR$minutes_remaining\033[0mmin,  score=$score"
-echo "voting  IP=$VOTE_IP"
-echo "current IP=$CUR_IP"
+echo " voting  IP=$VOTE_IP"
+echo " current IP=$CUR_IP"
 echo '--'
 echo -e " vote account:      $VOTE_ACC_ADDR"
 echo -e " epmty_keypair:     $GRAY$EMPTY_ADDR \033[0m"   
