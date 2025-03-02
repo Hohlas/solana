59a40878610e0d351d54838b5f109ac27d7467dc
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-03-02 22:33:24 +0300

    Update git_clone.sh

--- setup/git_clone.sh
+++ setup/git_clone.sh
@@ -17,16 +17,16 @@ chmod +x ~/sol_git/telegram_bot/watch_main.sh
 BASHRC_FILE="$HOME/.bashrc"
 NEW_ALIAS="alias guard='source ~/sol_git/guard/guard.sh'"
 
-if [ -f "$BASHRC_FILE" ]; then
+#if [ -f "$BASHRC_FILE" ]; then
     # Заменяем строку, если она существует, или добавляем её, если строки нет
-    if grep -q "^alias guard" "$BASHRC_FILE"; then
+    #if grep -q "^alias guard" "$BASHRC_FILE"; then
         # Заменяем строку, начинающуюся с "alias guard"
         #sed -i.bak "s|^alias guard.*|$NEW_ALIAS|" "$BASHRC_FILE"
         #echo "Строка успешно заменена на: $NEW_ALIAS"
-    else
-        echo "Строка 'alias guard' не найдена. Добавьте её вручную."
-    fi
-else
-    echo "Файл $BASHRC_FILE не найден."
-fi
+    #else
+       # echo "Строка 'alias guard' не найдена. Добавьте её вручную."
+    #fi
+#else
+    #echo "Файл $BASHRC_FILE не найден."
+#fi
 source ~/.bashrc
