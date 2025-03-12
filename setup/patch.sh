78f6f8517c6df320aa27723558309e1862006753
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-03-12 17:48:02 +0300

    Update patch.sh

--- setup/patch.sh
+++ setup/patch.sh
@@ -24,16 +24,16 @@ while true; do
     # Проверка попадания в заданные диапазоны
     if (( $(echo "$epoch_percent > 99.7" | bc -l) )); then
         #rm -f $set_file;                 echo "$(TIME) epoch=$epoch_percent% > 99.7: Path OFF"
-        echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% > 99.7: [$(cat $set_file)]" 
+        echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% > 99.7: soft[$(cat $set_file)]" 
     elif (( $(echo "$epoch_percent > 25.5" | bc -l) )); then
-        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% > 25.5: [$(cat $set_file)]"
+        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% > 25.5: hard[$(cat $set_file)]"
     elif (( $(echo "$epoch_percent > 24.5" | bc -l) )); then
         #rm -f $set_file;                 echo "$(TIME) epoch=$epoch_percent% > 24.5: Path OFF"
-        echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% > 24.5: [$(cat $set_file)]"
+        echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% > 24.5: soft[$(cat $set_file)]"
     elif (( $(echo "$epoch_percent > 0.5" | bc -l) )); then
-        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% >  0.5: [$(cat $set_file)]"
+        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% >  0.5: hard[$(cat $set_file)]"
     else
-        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% [$(cat $set_file)]"
+        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% hard[$(cat $set_file)]"
     fi
 
     sleep 60
