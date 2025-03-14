590146dd420c8963780a79b13a1ee0df0c0e1c1c
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-03-14 15:55:03 +0300

    Update patch.sh

--- setup/patch.sh
+++ setup/patch.sh
@@ -24,11 +24,11 @@ while true; do
     # Проверка попадания в заданные диапазоны
     if (( $(echo "$epoch_percent > 99.7" | bc -l) )); then
         echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% > 99.7: soft[$(cat $set_file)]" 
-    elif (( $(echo "$epoch_percent > 25.5" | bc -l) )); then
+    elif (( $(echo "$epoch_percent > 25.3" | bc -l) )); then
         echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% > 25.5: hard[$(cat $set_file)]"
-    elif (( $(echo "$epoch_percent > 24.5" | bc -l) )); then
+    elif (( $(echo "$epoch_percent > 24.8" | bc -l) )); then
         echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% > 24.5: soft[$(cat $set_file)]"
-    elif (( $(echo "$epoch_percent > 0.5" | bc -l) )); then
+    elif (( $(echo "$epoch_percent > 0.3" | bc -l) )); then
         echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% >  0.5: hard[$(cat $set_file)]"
     elif (( $(echo "$epoch_percent > 0" | bc -l) )); then
         echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% > 0: soft[$(cat $set_file)]"    
