eeb4df6d9b0afca036831538fc94b11f0072a41f
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-03-22 12:15:08 +0300

    Update patch.sh

--- setup/patch.sh
+++ setup/patch.sh
@@ -23,11 +23,11 @@ while true; do
 
     # Проверка попадания в заданные диапазоны
     if (( $(echo "$epoch_percent > 25.3" | bc -l) )); then
-        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% > 25.5: hard[$(cat $set_file)]"
+        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% > 25: hard[$(cat $set_file)]"
     elif (( $(echo "$epoch_percent > 24.8" | bc -l) )); then
-        echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% > 24.5: soft[$(cat $set_file)]"
+        echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% = 25: soft[$(cat $set_file)]"
     else
-        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% hard[$(cat $set_file)]"
+        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% < 25: hard[$(cat $set_file)]"
     fi
 
     sleep 60
