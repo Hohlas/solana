23df7335edae3589f5dab3f7c4d0f52cb93b5920
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-03-29 11:18:46 +0300

    Update git_clone.sh

--- setup/git_clone.sh
+++ setup/git_clone.sh
@@ -9,6 +9,7 @@ else
   git clone https://github.com/Hohlas/solana.git ~/sol_git
 fi
 chmod +x ~/sol_git/setup/*.sh
+chmod +x ~/sol_git/guard/*.sh
 chmod +x ~/sol_git/telegram_bot/watch_test.sh
 chmod +x ~/sol_git/telegram_bot/watch_main.sh
 
@@ -16,13 +17,13 @@ chmod +x ~/sol_git/telegram_bot/watch_main.sh
 
 BASHRC_FILE="$HOME/.bashrc"
 #NEW_ALIAS="alias patch='~/sol_git/setup/patch.sh'" # алиас на замену
-NEW_ALIAS="alias mon='~/sol_git/setup/mon.sh'" # алиас на замену
+NEW_ALIAS="alias behind='source ~/sol_git/guard/behind.sh'" # алиас на замену
 OLD_ALIAS="${NEW_ALIAS%%=*}" # Вырезаем все после первого "="
 
 : '
 многострочный 
 комментарий
-
+'
 
 if [ -f "$BASHRC_FILE" ]; then
     # Заменяем строку, если она существует, или добавляем её, если строки нет
@@ -37,5 +38,5 @@ if [ -f "$BASHRC_FILE" ]; then
 else
     echo "Файл $BASHRC_FILE не найден."
 fi
-'
+
 source ~/.bashrc
