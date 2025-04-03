004ab60b14836998881ac312b779d79ad9ebb4f8
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-04-03 09:36:37 +0300

    Update patch.sh

--- setup/patch.sh
+++ setup/patch.sh
@@ -3,8 +3,8 @@ PATCH_VER=v1.1.2
 #===================++++++++++========================
 # Настройки
 #orig="0.45 4 0 24" # настройки Шиноби
-hard="0.5 4 0 24" # чуть более консервативные настройки
-#hard="0.45 4 0 24" # чуть более консервативные настройки
+#hard="0.5 4 0 24" # чуть более консервативные настройки
+hard="0.45 4 0 24" # стандартные настройки
 soft="0.6 2 0 24" # очень консервативные настройки
 set_file="$HOME/solana/mostly_confirmed_threshold"
 GRAY=$'\033[90m'; GREEN=$'\033[32m'; RED=$'\033[31m'; YELLOW=$'\033[33m'; BLUE=$'\033[34m'; CLEAR=$'\033[0m'
@@ -26,16 +26,25 @@ while true; do
     fi
 
     # Проверка попадания в заданные диапазоны
+    # <0%
     if (( $(echo "$epoch_percent > 99.7" | bc -l) )); then
         echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% > 99: soft[$(cat $set_file)]" 
+    # 25%
     elif (( $(echo "$epoch_percent > 25.3" | bc -l) )); then
         echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% > 25: hard[$(cat $set_file)]"
     elif (( $(echo "$epoch_percent > 24.8" | bc -l) )); then
         echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% = 25: soft[$(cat $set_file)]"
+    # 65%
+    elif (( $(echo "$epoch_percent > 65.3" | bc -l) )); then
+        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% > 25: hard[$(cat $set_file)]"
+    elif (( $(echo "$epoch_percent > 64.8" | bc -l) )); then
+        echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% = 25: soft[$(cat $set_file)]"
+    # >0%    
     elif (( $(echo "$epoch_percent > 0.3" | bc -l) )); then
         echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% < 25: hard[$(cat $set_file)]"
     elif (( $(echo "$epoch_percent > 0" | bc -l) )); then
         echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% = 0: soft[$(cat $set_file)]"    
+    
     else
         echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% hard[$(cat $set_file)]"
     fi
