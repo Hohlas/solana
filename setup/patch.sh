99fd715c247be08ed6c64afd198c4d387b0834c5
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-04-03 09:55:40 +0300

    Update patch.sh

--- setup/patch.sh
+++ setup/patch.sh
@@ -36,9 +36,9 @@ while true; do
         echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% = 25: soft[$(cat $set_file)]"
     # 65%
     elif (( $(echo "$epoch_percent > 65.3" | bc -l) )); then
-        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% > 25: hard[$(cat $set_file)]"
+        echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% > 65: hard[$(cat $set_file)]"
     elif (( $(echo "$epoch_percent > 64.8" | bc -l) )); then
-        echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% = 25: soft[$(cat $set_file)]"
+        echo $soft > $set_file; echo "$(TIME) epoch=$epoch_percent% = 65: soft[$(cat $set_file)]"
     # >0%    
     elif (( $(echo "$epoch_percent > 0.3" | bc -l) )); then
         echo $hard > $set_file; echo "$(TIME) epoch=$epoch_percent% < 25: hard[$(cat $set_file)]"
