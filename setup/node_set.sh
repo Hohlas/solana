e6a2682d28254c80f4fd99e7a224ceffa1b25bc9
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-04-23 08:50:41 +0300

    Update node_set.sh
    test to agave

--- setup/node_set.sh
+++ setup/node_set.sh
@@ -40,7 +40,7 @@ if [[ $NODE == "main" ]]; then
     fi
 elif [[ $NODE == "test" ]]; then
     solana config set --url https://api.testnet.solana.com --keypair ~/solana/validator-keypair.json
-    cp ~/sol_git/firedancer/solana.service ~/solana/solana.service
+    cp ~/sol_git/test/solana.service ~/solana/solana.service
     export NAME=$(echo "$NAME" | tr '[:upper:]' '[:lower:]') # имя малыми буквами
 else
     echo -e "\033[31m Warning, unknown node type: $NODE \033[0m"
