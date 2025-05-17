f7a217a39160d60bf60f933c6c0a1386aa00a339
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-05-17 11:24:25 +0300

    Update git_clone.sh

--- setup/git_clone.sh
+++ setup/git_clone.sh
@@ -16,36 +16,27 @@ chmod +x ~/sol_git/telegram_bot/watch_main.sh
 # ##########################################
 
 
+BASHRC_FILE="$HOME/.bashrc"
+OLD_ALIAS="alias logs='tail -f ~/solana/solana.log'"
+NEW_ALIAS="alias logs='~/sol_git/setup/logs.sh'"
+
 : '
 многострочный 
 комментарий
 '
 
-RED='\e[31m'
-GREEN='\e[32m'
-YELLOW='\e[33m'
-CLEAR='\e[0m'
-
-# Определение переменных
-BASHRC_FILE="$HOME/.bashrc"
-NEW_ALIAS="alias logs='~/sol_git/setup/logs.sh'"
-OLD_ALIAS="alias logs='tail -f ~/solana/solana.log'"
-
-if grep -q "^alias logs=" "$BASHRC_FILE"; then
-        # Экранируем символы в OLD_ALIAS и NEW_ALIAS для sed
-        ESCAPED_OLD_ALIAS=$(echo "$OLD_ALIAS" | sed 's/[\/&]/\\&/g')
-        ESCAPED_NEW_ALIAS=$(echo "$NEW_ALIAS" | sed 's/[\/&]/\\&/g')
-        # Заменяем старую строку на новую
-        sed -i.bak "s|^alias logs=.*|$ESCAPED_NEW_ALIAS|" "$BASHRC_FILE"
-        echo -e "${GREEN}Alias [$OLD_ALIAS] replaced with: [$NEW_ALIAS]${CLEAR}"
+if [ -f "$BASHRC_FILE" ]; then
+    # Заменяем строку, если она существует, или добавляем её, если строки нет
+    if grep -q "^$OLD_ALIAS" "$BASHRC_FILE"; then
+        # Заменяем строку, начинающуюся с OLD_ALIAS
+        sed -i.bak "s|^$OLD_ALIAS.*|$NEW_ALIAS|" "$BASHRC_FILE"
+        echo "алиас [$OLD_ALIAS] заменен на: [$NEW_ALIAS]"
     else
-        # Добавляем новую строку, если алиас не найден
         echo "$NEW_ALIAS" >> "$BASHRC_FILE"
-        echo -e "${GREEN}Added new alias: [$NEW_ALIAS]${CLEAR}"
+        echo "добавлен новый алиас: [$NEW_ALIAS]"
     fi
-
-    
-
+else
+    echo "Файл $BASHRC_FILE не найден."
+fi
 
 source ~/.bashrc
-echo "source ~/.bashrc"
