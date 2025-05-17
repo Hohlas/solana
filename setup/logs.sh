2aef3ea506243f1fa1f413135d7989b140d4c431
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-05-17 10:53:51 +0300

    Create logs.sh

---
+++ setup/logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# colors
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+CLEAR='\e[0m'
+
+LOG_FILE="$HOME/solana/solana.log"
+
+if [ ! -f "$LOG_FILE" ]; then
+  echo -e "${RED}Error: Log file $LOG_FILE does not exist${CLEAR}"
+  exit 1
+fi
+
+# check arguments
+if [ -z "$1" ]; then
+  echo -e "${YELLOW} logs from $LOG_FILE${CLEAR}"
+  tail -f "$LOG_FILE"
+else
+  echo -e "${YELLOW} logs from $LOG_FILE grep '$1'${CLEAR}"
+  tail -f "$LOG_FILE" | grep --color=auto -i "$1"
+fi
