7931e02a811ab2c6dd123aadcbc8e13ed961f21e
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-06-06 08:30:44 +0300

    Update git_clone.sh

--- setup/git_clone.sh
+++ setup/git_clone.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
+# solana-setup
 if [ -d ~/sol_git ]; then 
   cd ~/sol_git; 
   git fetch origin; # get last updates from git
-  #git diff # просмотреть изменения, 
-  #git merge # применить изменения к локальному репу
   git reset --hard origin/main # сбросить локальную ветку до последнего коммита из git
 else 
   git clone https://github.com/Hohlas/solana.git ~/sol_git
 fi
 chmod +x ~/sol_git/setup/*.sh
-chmod +x ~/sol_git/guard/*.sh
 chmod +x ~/sol_git/telegram_bot/watch_test.sh
 chmod +x ~/sol_git/telegram_bot/watch_main.sh
+
+# solana-guard
+if [ -d ~/solana-guard ]; then 
+  cd ~/solana-guard; 
+  git fetch origin; # get last updates from git
+  git reset --hard origin/main # сбросить локальную ветку до последнего коммита из git
+else 
+  git clone https://github.com/Hohlas/solana-guard.git ~/solana-guard
+fi
+chmod +x ~/solana-guard/guard.sh
+chmod +x ~/solana-guard/check.sh
 
 ########  ADD/CHANGE  ALIAS  #########
 
 BASHRC_FILE="$HOME/.bashrc"
-OLD_ALIAS="alias logs='tail -f ~/solana/solana.log'"
-NEW_ALIAS="alias logs='~/sol_git/setup/logs.sh'"
+OLD_ALIAS="alias guard='source ~/sol_git/guard/guard.sh'"
+NEW_ALIAS="alias guard='source ~/solana-guard/guard.sh'"
 
 : '
 многострочный 
@@ -39,5 +48,8 @@ if [ -f "$BASHRC_FILE" ]; then
 else
     echo "file $BASHRC_FILE not found."
 fi
+
+
+
 
 echo "run: source ~/.bashrc"
