a1d8b88fae77e740c2cb4f4539f6070b637f422d
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2025-07-13 15:07:42 +0300

    Update git_clone.sh

--- setup/git_clone.sh
+++ setup/git_clone.sh
@@ -10,8 +10,8 @@ else
     git clone https://github.com/Hohlas/solana.git ~/sol_git
 fi
 chmod +x ~/sol_git/setup/*.sh
-chmod +x ~/sol_git/telegram_bot/watch_test.sh
-chmod +x ~/sol_git/telegram_bot/watch_main.sh
+#chmod +x ~/sol_git/telegram_bot/watch_test.sh
+#chmod +x ~/sol_git/telegram_bot/watch_main.sh
 
 # solana-guard
 if [ -d ~/solana-guard ]; then 
